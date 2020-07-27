dd4b5563fa76e8ca9354ee83364b60e89f8ed78d
Ben Straub <ben@straub.cc>
2020-07-27 14:00:18 -0700

    Partway through tutorial

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -27,9 +27,6 @@ export class SimpleActorSheet extends ActorSheet {
   getData () {
     const data = super.getData()
     data.dtypes = ['String', 'Number', 'Boolean']
-    for (let attr of Object.values(data.data.attributes)) {
-      attr.isCheckbox = attr.dtype === 'Boolean'
-    }
     return data
   }
 
@@ -55,94 +52,5 @@ export class SimpleActorSheet extends ActorSheet {
       this.actor.deleteOwnedItem(li.data('itemId'))
       li.slideUp(200, () => this.render(false))
     })
-
-    // Add or Remove Attribute
-    html
-      .find('.attributes')
-      .on(
-        'click',
-        '.attribute-control',
-        this._onClickAttributeControl.bind(this)
-      )
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  setPosition (options = {}) {
-    const position = super.setPosition(options)
-    const sheetBody = this.element.find('.sheet-body')
-    const bodyHeight = position.height - 192
-    sheetBody.css('height', bodyHeight)
-    return position
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Listen for click events on an attribute control to modify the composition of attributes in the sheet
-   * @param {MouseEvent} event    The originating left click event
-   * @private
-   */
-  async _onClickAttributeControl (event) {
-    event.preventDefault()
-    const a = event.currentTarget
-    const action = a.dataset.action
-    const attrs = this.object.data.data.attributes
-    const form = this.form
-
-    // Add new attribute
-    if (action === 'create') {
-      const nk = Object.keys(attrs).length + 1
-      let newKey = document.createElement('div')
-      newKey.innerHTML = `<input type="text" name="data.attributes.attr${nk}.key" value="attr${nk}"/>`
-      newKey = newKey.children[0]
-      form.appendChild(newKey)
-      await this._onSubmit(event)
-    }
-
-    // Remove existing attribute
-    else if (action === 'delete') {
-      const li = a.closest('.attribute')
-      li.parentElement.removeChild(li)
-      await this._onSubmit(event)
-    }
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  _updateObject (event, formData) {
-    // Handle the free-form attributes list
-    const formAttrs = expandObject(formData).data.attributes || {}
-    const attributes = Object.values(formAttrs).reduce((obj, v) => {
-      let k = v['key'].trim()
-      if (/[\s\.]/.test(k))
-        return ui.notifications.error(
-          'Attribute keys may not contain spaces or periods'
-        )
-      delete v['key']
-      obj[k] = v
-      return obj
-    }, {})
-
-    // Remove attributes which are no longer used
-    for (let k of Object.keys(this.object.data.data.attributes)) {
-      if (!attributes.hasOwnProperty(k)) attributes[`-=${k}`] = null
-    }
-
-    // Re-combine formData
-    formData = Object.entries(formData)
-      .filter(e => !e[0].startsWith('data.attributes'))
-      .reduce(
-        (obj, e) => {
-          obj[e[0]] = e[1]
-          return obj
-        },
-        { _id: this.object._id, 'data.attributes': attributes }
-      )
-
-    // Update the Actor
-    return this.object.update(formData)
   }
 }
