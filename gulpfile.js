2b89724ba7fb6506aeceb14263abad033bd31466
Ben Straub <ben@straub.cc>
2020-08-20 07:01:24 -0700

    Rename stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,30 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
+const gulp = require('gulp')
+const less = require('gulp-less')
 
 /* ----------------------------------------- */
 /*  Compile LESS
 /* ----------------------------------------- */
 
-const SIMPLE_LESS = ["styles/*.less"];
-function compileLESS() {
-  return gulp.src("styles/simple.less")
+const SIMPLE_LESS = ['styles/*.less']
+function compileLESS () {
+  return gulp
+    .src('styles/ironsworn.less')
     .pipe(less())
-    .pipe(gulp.dest("./styles/"))
+    .pipe(gulp.dest('./styles/'))
 }
-const css = gulp.series(compileLESS);
+const css = gulp.series(compileLESS)
 
 /* ----------------------------------------- */
 /*  Watch Updates
 /* ----------------------------------------- */
 
-function watchUpdates() {
-  gulp.watch(SIMPLE_LESS, css);
+function watchUpdates () {
+  gulp.watch(SIMPLE_LESS, css)
 }
 
 /* ----------------------------------------- */
 /*  Export Tasks
 /* ----------------------------------------- */
 
-exports.default = gulp.series(
-  gulp.parallel(css),
-  watchUpdates
-);
-exports.css = css;
+exports.default = gulp.series(gulp.parallel(css), watchUpdates)
+exports.css = css
