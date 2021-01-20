8554d7aa74f6d9237eeee70679fba8906796839b
Ben Straub <ben@straub.cc>
2021-01-20 14:46:17 -0800

    Separate asset and move compendia

--- module/datasworn.js
+++ module/datasworn.js
@@ -1,28 +1,33 @@
 export async function importFromDatasworn () {
-  // Empty out the pack
-  const pack = game.packs.get('world.ironsworn-items')
-  const idsToDelete = pack.index.map(x => x._id)
-  for (const id of idsToDelete) {
-    await pack.deleteEntity(id)
+  // Empty out the packs
+  for (const key of ['world.ironsworn-items', 'world.ironsworn-assets']) {
+    const pack = game.packs.get(key)
+    await pack.render(true)
+    const idsToDelete = pack.index.map(x => x._id)
+    for (const id of idsToDelete) {
+      await pack.deleteEntity(id)
+    }
   }
 
   // Moves
+  const movesPack = game.packs.get('world.ironsworn-items')
   const movesJson = await fetch(
     '/systems/foundry-ironsworn/assets/moves.json'
   ).then(x => x.json())
   for (const move of movesJson) {
-    await pack.createEntity({
+    await movesPack.createEntity({
       type: 'move',
       ...move
     })
   }
 
   // Assets
+  const assetsPack = game.packs.get('world.ironsworn-assets')
   const assetsJson = await fetch(
     '/systems/foundry-ironsworn/assets/assets.json'
   ).then(x => x.json())
   for (const asset of assetsJson) {
-    await pack.createEntity({
+    await assetsPack.createEntity({
       type: 'asset',
       ...asset
     })
