59ae82bd362f73b03fa5f27cef3a4de357c5dea4
Ben Straub <ben@straub.cc>
2021-06-14 17:13:22 -0700

    Add edit mode to asset sheet

--- src/module/item/asset/assetsheet.ts
+++ src/module/item/asset/assetsheet.ts
@@ -1,3 +1,29 @@
 import { IronswornItemSheet } from '../item-sheet'
 
-export class AssetSheet extends IronswornItemSheet {}
+export class AssetSheet extends IronswornItemSheet {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      height: 600,
+    } as BaseEntitySheet.Options)
+  }
+
+  _getHeaderButtons() {
+    const ret = super._getHeaderButtons()
+    if (this.options.editable) {
+      ret.unshift({
+        class: 'ironsworn-toggle-edit-mode',
+        label: 'Edit',
+        icon: 'fas fa-edit',
+        onclick: (e) => this._toggleEditMode(e),
+      })
+    }
+    return ret
+  }
+
+  _toggleEditMode(e: JQuery.ClickEvent) {
+    e.preventDefault()
+
+    const currentValue = this.item.getFlag('foundry-ironsworn', 'edit-mode')
+    this.item.setFlag('foundry-ironsworn', 'edit-mode', !currentValue)
+  }
+}
