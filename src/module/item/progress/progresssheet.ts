30faa61350e3a1f2abe64a371f3bd89b44684a8e
Ben Straub <ben@straub.cc>
2021-06-19 06:43:16 -0700

    Wire up rank pips

--- src/module/item/progress/progresssheet.ts
+++ src/module/item/progress/progresssheet.ts
@@ -4,4 +4,24 @@ export class ProgressSheet extends IronswornItemSheet {
   get template() {
     return 'systems/foundry-ironsworn/templates/item/progress.hbs'
   }
+
+  activateListeners(html: JQuery) {
+    super.activateListeners(html)
+
+    html.find(`.ironsworn__progress__mark`).on('click', (e) => this._handleMarkProgress.call(this, e))
+    html.find(`.ironsworn__progress__fulfill`).on('click', (e) => this._handleFulfill.call(this, e))
+    html.find(`.ironsworn__progress__rank`).on('click', e => this._handleRankSet.call(this, e))
+  }
+
+  _handleMarkProgress(ev: JQuery.ClickEvent) {
+    ev.preventDefault()
+  }
+  _handleFulfill(ev: JQuery.ClickEvent) {
+    ev.preventDefault()
+  }
+  _handleRankSet(ev: JQuery.ClickEvent) {
+    ev.preventDefault()
+
+    this.item.update({'data.rank': ev.currentTarget.dataset.rank})
+  }
 }
