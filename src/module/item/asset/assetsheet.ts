5518f3dcc07965a5c01e4318d558a3f8df1de735
Ben Straub <ben@straub.cc>
2021-06-23 07:05:38 -0600

    Use custom handler for asset ability toggles

--- src/module/item/asset/assetsheet.ts
+++ src/module/item/asset/assetsheet.ts
@@ -1,10 +1,15 @@
 import { IronswornItemSheet } from '../item-sheet'
+import { AssetItemData } from '../itemtypes'
 
 export class AssetSheet extends IronswornItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       height: 600,
     } as BaseEntitySheet.Options)
+  }
+
+  activateListeners(html: JQuery) {
+    html.find('.ironsworn__ability__enable').on('click', (ev) => this._abilityToggle.call(this, ev))
   }
 
   _getHeaderButtons() {
@@ -25,5 +30,15 @@ export class AssetSheet extends IronswornItemSheet {
 
     const currentValue = this.item.getFlag('foundry-ironsworn', 'edit-mode')
     this.item.setFlag('foundry-ironsworn', 'edit-mode', !currentValue)
+  }
+
+  _abilityToggle(ev: JQuery.ClickEvent) {
+    ev.preventDefault()
+
+    const { idx } = ev.currentTarget.dataset
+    const assetData = this.item.data as AssetItemData
+    const abilities = Object.values(assetData.data.abilities)
+    abilities[idx].enabled = !abilities[idx].enabled
+    this.item.update({ data: { abilities } })
   }
 }
