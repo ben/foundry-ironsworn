9adb4d09bc805a13c2d7e2a4b76932c8ba560845
Ben Straub <ben@straub.cc>
2021-06-24 18:34:26 -0600

    Turn on editorconfig and reformat all source files

--- src/module/item/progress/progressitem.ts
+++ src/module/item/progress/progressitem.ts
@@ -9,7 +9,7 @@ export class ProgressItem extends BaseItem {
 
     html.find(`.ironsworn__progress__mark`).on('click', (e) => this._handleMarkProgress.call(this, e, sheet))
     html.find(`.ironsworn__progress__fulfill`).on('click', (e) => this._handleFulfill.call(this, e, sheet))
-    html.find(`.ironsworn__progress__rank`).on('click', e => this._handleRankSet.call(this, e, sheet))
+    html.find(`.ironsworn__progress__rank`).on('click', (e) => this._handleRankSet.call(this, e, sheet))
   }
 
   static _handleMarkProgress(ev, actorSheet) {
@@ -31,7 +31,7 @@ export class ProgressItem extends BaseItem {
   static _handleRankSet(ev, actorSheet) {
     ev.preventDefault()
 
-    const {item: itemId, rank} = ev.currentTarget.dataset
+    const { item: itemId, rank } = ev.currentTarget.dataset
     const item = actorSheet.actor.items.get(itemId) as IronswornItem
     item.setRank(rank)
   }
