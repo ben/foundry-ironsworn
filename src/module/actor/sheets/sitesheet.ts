4c1397736e3a70f27b89e3252bc880871ed46124
Ben Straub <ben@straub.cc>
2021-07-16 09:27:55 -0700

    Edit button and some data processing

--- src/module/actor/sheets/sitesheet.ts
+++ src/module/actor/sheets/sitesheet.ts
@@ -1,7 +1,17 @@
 import { IronswornSettings } from "../../helpers/settings"
+import { IronswornItem } from "../../item/item"
 import { SiteDataSource } from "../actortypes"
 
-export class IronswornSiteSheet extends ActorSheet {
+interface Data extends ActorSheet.Data<ActorSheet.Options> {
+  theme?: IronswornItem
+  domain?: IronswornItem
+}
+
+export class IronswornSiteSheet extends ActorSheet<ActorSheet.Options, Data> {
+  get siteData() {
+    return this.actor.data as SiteDataSource
+  }
+
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ['ironsworn', 'sheet', 'site', `theme-${IronswornSettings.theme}`],
@@ -11,16 +21,34 @@ export class IronswornSiteSheet extends ActorSheet {
     })
   }
 
-  get siteData() {
-    return this.actor.data as SiteDataSource
+  _getHeaderButtons() {
+    return [
+      {
+        class: 'ironsworn-toggle-edit-mode',
+        label: 'Edit',
+        icon: 'fas fa-edit',
+        onclick: (e) => this._toggleEditMode(e),
+      },
+      ...super._getHeaderButtons(),
+    ]
+  }
+
+  _toggleEditMode(e: JQuery.ClickEvent) {
+    e.preventDefault()
+
+    const currentValue = this.actor.getFlag('foundry-ironsworn', 'edit-mode')
+    this.actor.setFlag('foundry-ironsworn', 'edit-mode', !currentValue)
   }
 
   activateListeners(html: JQuery) {
     super.activateListeners(html)
   }
 
-  getData() {
-    const data = super.getData()
+  async getData() {
+    const data = await super.getData()
+
+    data.theme = this.actor.items.find(x => x.type === 'delve-theme')
+    data.domain = this.actor.items.find(x => x.type === 'delve-domain')
 
     return data
   }
