079650f8b91d8a8eb089e5923414fd23ccd482c1
Ben Straub <ben@straub.cc>
2021-07-17 14:21:52 -0700

    Register these items for auto wiring

--- src/module/item/delve-theme-domain/delvethemeitem.ts
+++ src/module/item/delve-theme-domain/delvethemeitem.ts
@@ -1,8 +1,8 @@
 import { IronswornCharacterSheet } from "../../actor/sheets/charactersheet";
 import { BaseItem } from "../baseitem";
 
-export class DelveDomainItem extends BaseItem {
-  static entityName = 'delve-domain'
+export class DelveThemeItem extends BaseItem {
+  static entityName = 'delve-theme'
 
   static activateActorSheetListeners(html: JQuery, sheet: IronswornCharacterSheet) {
     super.activateActorSheetListeners(html, sheet)
