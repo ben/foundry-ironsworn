a03d7d7bcab75ada16432a33f4a8b8b99a43934b
Ben Straub <ben@straub.cc>
2021-07-28 16:45:54 -0700

    Add bonus section to roll result

--- src/module/chat/movecontentcallbacks.ts
+++ src/module/chat/movecontentcallbacks.ts
@@ -24,4 +24,18 @@ export const MoveContentCallbacks: { [key: string]: Callback } = {
       Roll that table
     </button>
   `,
+
+  Sojourn: ({ hitType }: CallbackInput) =>
+    hitType === HIT_TYPE.MISS
+      ? ''
+      : `
+        <hr>
+        <p>On a hit, you and your allies may each focus on one of your chosen recover actions and roll +heart again. If you share a bond, add +1.</p>
+        <p>On a <strong>strong hit</strong>, take +2 more for that action.</p>
+        <p>On a <strong>weak hit</strong>, take +1 more.</p>
+        <p>On a <strong>miss</strong>, it goes badly and you lose all benefits for that action.</p>
+        <button class="ironsworn__sojourn__extra__roll">
+          <i class="fa fa-dice-d6"></i> Roll +heart
+        </button>
+      `,
 }
