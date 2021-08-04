55e099655a051ecccfda92143672231d6901ba34
Ben Straub <ben@straub.cc>
2021-08-04 08:02:07 -0700

    Skeleton for prompt dialog

--- src/module/applications/worldTruthsDialog.ts
+++ src/module/applications/worldTruthsDialog.ts
@@ -1,4 +1,4 @@
-import { IronswornSettings } from "../helpers/settings"
+import { IronswornSettings } from '../helpers/settings'
 
 export class WorldTruthsDialog extends FormApplication<FormApplication.Options> {
   constructor() {
@@ -21,8 +21,33 @@ export class WorldTruthsDialog extends FormApplication<FormApplication.Options> 
     // Nothing to do
   }
 
-  static maybeShow() {
+  static async maybeShow() {
     // Show this dialog if appropriate
-    new WorldTruthsDialog().render(true)
+    // TODO: return if dont-bug-me setting is set
+
+    const d = new Dialog({
+      title: game.i18n.localize('IRONSWORN.YourWorldTruths'),
+      content: '<p>Would you like to generate your world truths?</p>',
+      buttons: {
+        yes: {
+          icon: '<i class="fas fa-check"></i>',
+          label: 'Yes',
+          callback: () => new WorldTruthsDialog().render(true),
+        },
+        no: {
+          icon: '<i class="fas fa-times"></i>',
+          label: 'Not this time',
+        },
+        goaway: {
+          icon: '<i class="fas fa-times"></i>',
+          label: 'Never',
+          callback: () => {},
+        },
+      },
+      default: 'two',
+      render: (_html) => console.log('Register interactivity in the rendered dialog'),
+      close: (_html) => console.log('This always is logged no matter which option is chosen'),
+    })
+    d.render(true)
   }
 }
