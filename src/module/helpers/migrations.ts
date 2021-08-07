9dd965e96100a6b0b986d6f1051bd6ea674365e7
Ben Straub <ben@straub.cc>
2021-08-07 08:42:36 -0700

    Implement migration

--- src/module/helpers/migrations.ts
+++ src/module/helpers/migrations.ts
@@ -1,9 +1,32 @@
+import { IronswornActor } from "../actor/actor"
+import { IronswornItem } from "../item/item"
+
 function noop() {
   // no-op
 }
 
 async function fixFormidableSpelling() {
   // Iterate through everything that has a rank (sites, items, owned items), and change "formidible" to "formidable"
+  const setRank = async (x: IronswornActor | IronswornItem) => {
+    if ((x?.data?.data as any).rank === 'formidible') {
+      console.log(`Upgrading ${x.type} / ${x.name}`)
+      await x.update({data: {rank: 'formidable'}})
+    }
+  }
+  for (const item of game.items?.contents || []) {
+    await setRank(item)
+  }
+  for (const actor of game.actors?.contents || []) {
+    await setRank(actor)
+    for (const item of actor.items) {
+      await setRank(item)
+    }
+  }
+  for (const pack of game.packs.contents) {
+    for (const thing of pack.contents) {
+      await setRank(thing as any)
+    }
+  }
 }
 
 // index 1 is the function to run when upgrading from 1 to 2, and so on
@@ -30,7 +53,7 @@ export async function runDataMigrations() {
     }
 
     // All done
-    // game.settings.set('foundry-ironsworn', 'data-version', NEWEST_VERSION)
+    game.settings.set('foundry-ironsworn', 'data-version', NEWEST_VERSION)
     ui.notifications?.warn('All done! Carry on.', { permanent: true })
   } catch (e) {
     ui.notifications?.error("Whoops! That didn't work at all. Try reloading your browser to run it again.", { permanent: true })
