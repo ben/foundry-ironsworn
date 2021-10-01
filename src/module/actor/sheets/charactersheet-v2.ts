5ce3ba21c8b440e0697ac7823906d0e7b226e5f4
Ben Straub <ben@straub.cc>
2021-10-01 06:20:21 -0700

    Move sheet open/close wiring

--- src/module/actor/sheets/charactersheet-v2.ts
+++ src/module/actor/sheets/charactersheet-v2.ts
@@ -1,5 +1,6 @@
 import { IronswornSettings } from '../../helpers/settings'
 import { IronswornVueActorSheet } from '../vueactorsheet'
+import { CharacterMoveSheet } from './charactermovesheet'
 
 export class IronswornCharacterSheetV2 extends IronswornVueActorSheet {
   static get defaultOptions() {
@@ -17,11 +18,6 @@ export class IronswornCharacterSheetV2 extends IronswornVueActorSheet {
   getData() {
     let data: any = super.getData()
 
-    data.assets = this.actor.items.filter((x) => x.type === 'asset')
-    data.vows = this.actor.items.filter((x) => x.type === 'vow')
-    data.progresses = this.actor.items.filter((x) => x.type === 'progress')
-    data.bonds = this.actor.items.find((x) => x.type === 'bondset')
-
     // Allow every itemtype to add data to the actorsheet
     for (const itemType of CONFIG.IRONSWORN.itemClasses) {
       data = itemType.getActorSheetData(data, this)
@@ -33,6 +29,16 @@ export class IronswornCharacterSheetV2 extends IronswornVueActorSheet {
     return data
   }
 
+  render(...args) {
+    if (this._state <= Application.RENDER_STATES.NONE) this._openMoveSheet()
+    return super.render(...args)
+  }
+
+  close(...args) {
+    this.actor.moveSheet?.close(...args)
+    return super.close(...args)
+  }
+
   _getHeaderButtons() {
     return [
       {
@@ -41,12 +47,12 @@ export class IronswornCharacterSheetV2 extends IronswornVueActorSheet {
         icon: 'fas fa-edit',
         onclick: (e) => this._toggleEditMode(e),
       },
-      // {
-      //   class: 'ironsworn-open-move-sheet',
-      //   label: 'Moves',
-      //   icon: 'fas fa-directions',
-      //   onclick: (e) => this._openMoveSheet(e),
-      // },
+      {
+        class: 'ironsworn-open-move-sheet',
+        label: 'Moves',
+        icon: 'fas fa-directions',
+        onclick: (e) => this._openMoveSheet(e),
+      },
       ...super._getHeaderButtons(),
     ]
   }
@@ -54,5 +60,13 @@ export class IronswornCharacterSheetV2 extends IronswornVueActorSheet {
   _toggleEditMode(_e: JQuery.ClickEvent) {
     const currentValue = this.actor.getFlag('foundry-ironsworn', 'edit-mode')
     this.actor.setFlag('foundry-ironsworn', 'edit-mode', !currentValue)
+  }
+
+  _openMoveSheet(_e?: JQuery.ClickEvent) {
+    if (this.actor.moveSheet) {
+      this.actor.moveSheet.render(true, { focus: true })
+    } else {
+      new CharacterMoveSheet(this.actor).render(true)
+    }
   }
 }
