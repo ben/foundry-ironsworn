9d15f82970d4f9019e5d8d097076b46a772b5225
Ben Straub <ben@straub.cc>
2021-10-06 19:33:27 -0700

    First stab at boxrow component

---
+++ src/module/vue/components/boxrow/boxrow.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="boxgroup">
+    <div class="flexrow boxrow">
+      <boxrow-box
+        v-for="(item, i) in items"
+        :key="i"
+        v-bind="item"
+        @click="click"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      validator(val) {
+        return val.every((x) => x?.text !== undefined && x?.value !== undefined)
+      },
+    },
+  },
+
+  methods: {
+    click(el, value) {
+      this.$emit('click', el, value
+      )
+    }
+  }
+}
+</script>
+
--- src/module/vue/shared-sheet.vue
+++ src/module/vue/shared-sheet.vue
@@ -1,28 +1,61 @@
 <template>
-  <header class="sheet-header">
-    <img
-      :src="actor.img"
-      :title="actor.name"
-      class="profile-img"
-      data-edit="img"
-      height="50"
-      width="50"
-    />
-    <h1 class="charname">
-      <input
-        :placeholder="$t('IRONSWORN.Name')"
-        v-model="actor.name"
-        name="name"
-        type="text"
+  <div class="flexcol">
+    <header class="sheet-header">
+      <img
+        :src="actor.img"
+        :title="actor.name"
+        class="profile-img"
+        data-edit="img"
+        height="50"
+        width="50"
       />
-    </h1>
-  </header>
+      <h1 class="charname">
+        <input
+          :placeholder="$t('IRONSWORN.Name')"
+          v-model="actor.name"
+          name="name"
+          type="text"
+        />
+      </h1>
+    </header>
+
+    <section class="sheet-area">
+      <h2 class="clickable text nogrow">
+        {{ $t('IRONSWORN.Supply') }}
+      </h2>
+
+      <boxrow
+        class="nogrow"
+        style="line-height: 25px"
+        :items="supplyItems"
+        @click="supplyClick"
+      />
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     actor: Object,
+  },
+
+  computed: {
+    supplyItems() {
+      const current = this.actor.data.supply
+      return [
+        { text: '0', value: 0, selected: current === 0 },
+        { text: '1', value: 1, selected: current === 1 },
+        { text: '2', value: 2, selected: current === 2 },
+        { text: '3', value: 3, selected: current === 3 },
+        { text: '4', value: 4, selected: current === 4 },
+        { text: '5', value: 5, selected: current === 5 },
+      ]
+    },
+  },
+
+  methods: {
+    supplyClick: console.log,
   },
 }
 </script>
