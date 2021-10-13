b9ecd1b7e3cfd7fdafe6f2aba23716ca5a78f132
Ben Straub <ben@straub.cc>
2021-10-13 07:05:57 -0700

    Name + description

--- src/module/vue/asset-sheet.vue
+++ src/module/vue/asset-sheet.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="flexcol">
-    <h2>Asset {{ item.name }}</h2>
+    <header class="sheet-header nogrow">
+      <h1 class="charname">
+        <input type="text" v-model="item.name" />
+      </h1>
+    </header>
+
+    <p class="nogrow">
+      <input v-if="editMode" type="text" v-model="item.data.description" />
+      <span v-else>{{ item.data.description }}</span>
+    </p>
   </div>
 </template>
 
@@ -9,5 +18,22 @@ export default {
   props: {
     item: Object,
   },
+
+  computed: {
+    editMode() {
+      return this.item.flags['foundry-ironsworn']?.['edit-mode']
+    },
+  },
+
+  watch: {
+    'item.name': CONFIG.IRONSWORN._.debounce(function () {
+      this.$item().update({ name: this.item.name })
+    }, 1000),
+    'item.data.description': CONFIG.IRONSWORN._.debounce(function () {
+      this.$item().update({ data: { description: this.item.data.description } })
+    }, 1000),
+  },
+
+  methods: {},
 }
 </script>
