ef3ef3327151123c5bfdd7cee8f40a6bef23bda7
Ben Straub <ben@straub.cc>
2021-10-16 07:10:01 -0700

    New components to DRY up sheets

---
+++ src/module/vue/components/document-img.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    :src="document.img"
+    :title="document.name"
+    class="profile-img"
+    data-edit="img"
+    height="50"
+    width="50"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    document: Object
+  }
+}
+</script>
---
+++ src/module/vue/components/document-name.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="charname">
+    <input
+      :placeholder="$t('IRONSWORN.Name')"
+      v-model="document.name"
+      name="name"
+      type="text"
+    />
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    document: Object,
+  },
+
+  methods: {
+    save() {
+      const document = this.$item ?? this.$actor
+      document?.update({ name: this.document.name })
+    },
+  },
+}
+</script>
