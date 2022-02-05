705bb0f2cbe87642a766749f3eaaf71635374d3b
Ben Straub <ben@straub.cc>
2022-02-05 15:00:30 -0800

    Rough in stats

--- src/module/vue/sf-charactersheet.vue
+++ src/module/vue/sf-charactersheet.vue
@@ -1,5 +1,76 @@
 <template>
-  <h1>Starforged Character Sheet</h1>
+  <div class="flexcol">
+    <!-- Header row -->
+    <character-header :actor="actor"></character-header>
+
+    <!-- Main body row -->
+    <div class="flexrow">
+      <!-- Momentum on left -->
+      <div class="flexcol margin-left">
+        <div class="flexrow" style="flex-wrap: nowrap">
+          <div class="flexcol stack momentum">
+            <stack
+              :actor="actor"
+              stat="momentum"
+              :top="10"
+              :bottom="-6"
+              :softMax="actor.data.momentumMax"
+            ></stack>
+            <hr class="nogrow" />
+            <div class="nogrow">
+              <div class="clickable block stack-row" @click="burnMomentum">
+                {{ $t('IRONSWORN.Burn') }}
+              </div>
+            </div>
+
+            {{ $t('IRONSWORN.Reset') }}: {{ actor.data.momentumReset }}
+            {{ $t('IRONSWORN.Max') }}: {{ actor.data.momentumMax }}
+          </div>
+
+          <h4 class="vertical-v2">{{ $t('IRONSWORN.Momentum') }}</h4>
+        </div>
+      </div>
+
+      <!-- Center area -->
+      <div class="flexcol">
+        <!-- TODO: -->
+      </div>
+
+      <!-- Stats on right -->
+      <div class="flexcol margin-right">
+        <div class="flexrow nogrow" style="flex-wrap: nowrap">
+          <h4 class="vertical-v2 clickable text" @click="rollStat('health')">
+            {{ $t('IRONSWORN.Health') }}
+          </h4>
+          <div class="flexcol stack health">
+            <stack :actor="actor" stat="health" :top="5" :bottom="0"></stack>
+          </div>
+        </div>
+
+        <hr class="nogrow" />
+
+        <div class="flexrow nogrow" style="flex-wrap: nowrap">
+          <h4 class="vertical-v2 clickable text" @click="rollStat('spirit')">
+            {{ $t('IRONSWORN.Spirit') }}
+          </h4>
+          <div class="flexcol stack spirit">
+            <stack :actor="actor" stat="spirit" :top="5" :bottom="0"></stack>
+          </div>
+        </div>
+
+        <hr class="nogrow" />
+
+        <div class="flexrow nogrow" style="flex-wrap: nowrap">
+          <h4 class="vertical-v2 clickable text" @click="rollStat('supply')">
+            {{ $t('IRONSWORN.Supply') }}
+          </h4>
+          <div class="flexcol stack supply">
+            <stack :actor="actor" stat="supply" :top="5" :bottom="0"></stack>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
