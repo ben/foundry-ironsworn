6813998dd2cb89e00d451f6c65314ddfd85fb69c
Ben Straub <ben@straub.cc>
2022-02-06 12:28:07 -0800

    Rough progress display

--- src/module/vue/components/legacy-track.vue
+++ src/module/vue/components/legacy-track.vue
@@ -1,8 +1,32 @@
 <template>
-  <h4>legacy track: {{ ticks }} {{ xpSpent }}</h4>
+  <div class="flexrow track">
+    <div
+      class="flexcol track-box"
+      v-for="(box, i) in boxes"
+      :key="`box${i}`"
+      v-html="box"
+    ></div>
+  </div>
 </template>
 
+<style lang="less" scoped>
+// .track .track-box {
+//   width: 40px;
+//   height: 40px;
+//   flex-grow: 0;
+// }
+</style>
+
 <script>
+function ticksSvg(ticks) {
+  let ret = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">'
+  if (ticks > 0) ret += '<line x1="23" y1="23" x2="77" y2="77" />'
+  if (ticks > 1) ret += '<line x1="77" y1="23" x2="23" y2="77" />'
+  if (ticks > 2) ret += '<line x1="15" y1="50" x2="85" y2="50" />'
+  if (ticks > 3) ret += '<line x1="50" y1="15" x2="50" y2="85" />'
+  return ret + '</svg>'
+}
+
 export default {
   props: {
     actor: Object,
@@ -11,10 +35,22 @@ export default {
 
   computed: {
     ticks() {
-      return this.actor.data.legacies[this.propKey] || 0
+      return this.actor.data.legacies[this.propKey] ?? 0
     },
     xpSpent() {
-      return this.actor.data.legacies[`${this.propKey}XpSpent`] || 0
+      return this.actor.data.legacies[`${this.propKey}XpSpent`] ?? 0
+    },
+    overflow() {
+      return Math.floor(this.ticks / 40) * 10
+    },
+    boxes() {
+      const ret = []
+      let remainingTicks = this.ticks % 40
+      for (let i = 0; i < 10; i++) {
+        ret.push(ticksSvg(remainingTicks))
+        remainingTicks -= 4
+      }
+      return ret
     },
   },
 }
--- src/module/vue/components/sf-tabs/sf-legacies.vue
+++ src/module/vue/components/sf-tabs/sf-legacies.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <h3>Legacies</h3>
-
+    <h4>{{$t('IRONSWORN.Quests')}}</h4>
     <legacy-track :actor="actor" propKey="quests" />
+
+    <hr>
+    <h4>{{$t('IRONSWORN.Bonds')}}</h4>
     <legacy-track :actor="actor" propKey="bonds" />
+
+    <hr>
+    <h4>{{$t('IRONSWORN.Discoveries')}}</h4>
     <legacy-track :actor="actor" propKey="discoveries" />
   </div>
 </template>
