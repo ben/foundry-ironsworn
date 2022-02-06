91be65ecad67f92fa7404ad8c9471e63edf69c44
Ben Straub <ben@straub.cc>
2022-02-06 08:07:13 -0800

    New legacy-track component

---
+++ src/module/vue/components/legacy-track.vue
@@ -0,0 +1,21 @@
+<template>
+  <h4>legacy track: {{ ticks }} {{ xpSpent }}</h4>
+</template>
+
+<script>
+export default {
+  props: {
+    actor: Object,
+    propKey: String,
+  },
+
+  computed: {
+    ticks() {
+      return this.actor.data.legacies[this.propKey] || 0
+    },
+    xpSpent() {
+      return this.actor.data.legacies[`${this.propKey}XpSpent`] || 0
+    },
+  },
+}
+</script>
--- src/module/vue/components/sf-tabs/sf-legacies.vue
+++ src/module/vue/components/sf-tabs/sf-legacies.vue
@@ -1,3 +1,18 @@
 <template>
-  <h3>Legacies</h3>
+  <div>
+    <h3>Legacies</h3>
+
+    <legacy-track :actor="actor" propKey="quests" />
+    <legacy-track :actor="actor" propKey="bonds" />
+    <legacy-track :actor="actor" propKey="discoveries" />
+  </div>
 </template>
+
+<script>
+import LegacyTrack from '../legacy-track.vue'
+export default {
+  props: {
+    actor: Object,
+  },
+}
+</script>
