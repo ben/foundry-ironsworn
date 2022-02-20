76c7ea0ff1d40c59f730f71c7dd6fe926dcd30de
Ben Straub <ben@straub.cc>
2022-02-19 18:06:26 -0800

    Make room on the sheet for clocks

--- src/module/vue/progress-sheet.vue
+++ src/module/vue/progress-sheet.vue
@@ -17,23 +17,50 @@
       <option value="bond">{{ $t('IRONSWORN.Bond') }}</option>
     </select>
 
-    <!-- RANK -->
-    <div class="flexrow nogrow">
-      <rank-hexes
-        :current="item.data.rank"
-        @click="setRank"
-        class="nogrow"
-        style="margin-right: 1em"
-      />
-      <h4>{{ rankText }}</h4>
-      <icon-button v-if="editMode" icon="trash" @click="clearProgress" />
-      <icon-button icon="play" @click="markProgress" />
+    <div class="nogrow">
+      <label class="checkbox">
+        <input type="checkbox" v-model="item.data.hasTrack" />
+        Progress Track
+      </label>
+
+      <transition name="slide">
+        <div class="nogrow" v-if="item.data.hasTrack">
+          <!-- RANK -->
+          <div class="flexrow nogrow">
+            <rank-hexes
+              :current="item.data.rank"
+              @click="setRank"
+              class="nogrow"
+              style="margin-right: 1em"
+            />
+            <h4>{{ rankText }}</h4>
+            <icon-button v-if="editMode" icon="trash" @click="clearProgress" />
+            <icon-button icon="play" @click="markProgress" />
+          </div>
+          <!-- PROGRESS -->
+          <div class="flexrow track nogrow" style="margin-bottom: 1em">
+            <progress-track :ticks="item.data.current" />
+          </div>
+        </div>
+      </transition>
     </div>
 
-    <!-- PROGRESS -->
-    <div class="flexrow track nogrow" style="margin-bottom: 1em">
-      <progress-track :ticks="item.data.current" />
+    <hr class="nogrow" />
+
+    <div class="nogrow">
+      <label class="checkbox">
+        <input type="checkbox" v-model="item.data.hasClock" />
+        Clock
+      </label>
+
+      <transition name="slide">
+        <div class="nogrow" v-if="item.data.hasClock">
+          (CLOCK)
+        </div>
+      </transition>
     </div>
+
+    <hr class="nogrow" />
 
     <!-- DESCRIPTION -->
     <editor
@@ -45,6 +72,27 @@
     />
   </div>
 </template>
+
+<style lang="less" scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.4s ease;
+  overflow: hidden;
+  max-height: 93px;
+  opacity: 1;
+}
+.slide-enter,
+.slide-leave-to {
+  max-height: 0;
+  opacity: 0;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  border-top: 0;
+  border-bottom: 0;
+}
+</style>
 
 <script>
 export default {
