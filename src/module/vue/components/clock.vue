8694b3c23210b73f0d3c70ae02867b5e77c11a40
Ben Straub <ben@straub.cc>
2022-02-20 08:30:27 -0800

    Hover highlights

--- src/module/vue/components/clock.vue
+++ src/module/vue/components/clock.vue
@@ -7,23 +7,32 @@
     viewBox="-55 -55 110 110"
   >
     <path
-      v-for="wedge in computedWedges"
+      v-for="(wedge, i) in computedWedges"
       :key="wedge.path"
       :d="wedge.path"
       fill="white"
       stroke="black"
       stroke-width="2"
       class="clickable svg"
+      :class="hoverClass(i)"
+      @mouseover="hover(i)"
+      @mouseleave="leave(i)"
     ></path>
   </svg>
 </template>
+
+<style lang="less" scoped>
+svg {
+  pointer-events: fill;
+}
+</style>
 
 <script>
 const R = 50
 
 function pathString(wedgeIdx, numWedges) {
   const wedgeAngle = (2 * Math.PI) / numWedges
-  const startAngle = wedgeIdx * wedgeAngle - (Math.PI / 2)
+  const startAngle = wedgeIdx * wedgeAngle - Math.PI / 2
   const x1 = R * Math.cos(startAngle)
   const y1 = R * Math.sin(startAngle)
   const x2 = R * Math.cos(startAngle + wedgeAngle)
@@ -43,6 +52,12 @@ export default {
     },
   },
 
+  data() {
+    return {
+      hovered: -1,
+    }
+  },
+
   computed: {
     computedWedges() {
       const ret = []
@@ -53,6 +68,21 @@ export default {
         })
       }
       return ret
+    },
+  },
+
+  methods: {
+    hover(i) {
+      console.log('hover', i)
+      this.hovered = i
+    },
+    leave(i) {
+      console.log('leave', i)
+      this.hovered = -1
+    },
+
+    hoverClass(i) {
+      return { hover: this.hovered >= i }
     },
   },
 }
