29e4274febf596516871b156380ea26d847f5361
Ben Straub <ben@straub.cc>
2022-02-27 08:21:36 -0800

    Quick search for moves

--- src/module/vue/components/sf-movesheetmoves.vue
+++ src/module/vue/components/sf-movesheetmoves.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="flexcol">
-    <div class="nogrow" v-for="ck of categoryKeys" :key="ck">
+    <div class="flexrow nogrow">
+      <input
+        type="text"
+        :placeholder="$t('IRONSWORN.Search')"
+        v-model="searchQuery"
+      />
+      <i
+        class="fa fa-times-circle nogrow clickable text"
+        @click="clearSearch"
+        style="padding: 6px"
+      />
+    </div>
+
+    <div class="nogrow" v-if="searchQuery">
+      <sf-moverow
+        v-for="move of searchResults"
+        :key="move.$id"
+        :actor="actor"
+        :move="move"
+      />
+    </div>
+
+    <div class="nogrow" v-else v-for="ck of categoryKeys" :key="ck">
       <h2>
         {{ ck }}
       </h2>
@@ -29,6 +51,7 @@ export default {
 
   data() {
     return {
+      searchQuery: '',
       moves: {},
       categoryKeys: [
         'Session Moves',
@@ -63,9 +86,30 @@ export default {
     this.moves = moves
   },
 
+  computed: {
+    sortedMoves() {
+      const ret = []
+      for (const category of Object.keys(this.moves || {})) {
+        ret.push(...this.moves[category].moves)
+      }
+      return ret
+    },
+
+    searchResults() {
+      if (!this.searchQuery) return null
+
+      const re = new RegExp(this.searchQuery, 'i')
+      return this.sortedMoves.filter((x) => re.test(x.foundryItem.name))
+    },
+  },
+
   methods: {
     movesForKey(ck) {
       return this.moves[ck]?.moves
+    },
+
+    clearSearch() {
+      this.searchQuery = ''
     },
   },
 }
