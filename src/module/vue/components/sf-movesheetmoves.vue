d7218b265620572fc98dd66e5cb8699adb32bd8d
Ben Straub <ben@straub.cc>
2022-02-26 17:40:53 -0800

    Clean up error on first open

--- src/module/vue/components/sf-movesheetmoves.vue
+++ src/module/vue/components/sf-movesheetmoves.vue
@@ -5,7 +5,7 @@
         {{ ck }}
       </h2>
 
-      <div class="item-row" v-for="move of moves[ck].moves" :key="move.key">
+      <div class="item-row" v-for="move of movesForKey(ck)" :key="move.key">
         <h4 style="margin: 0">
           <i class="fa fa-dice-d6" />
           {{ move.Name }}
@@ -48,15 +48,20 @@ export default {
 
   async created() {
     const json = await fetchJson()
-    const categorizedMoves = {}
+    const moves = {}
     for (const move of json) {
-      categorizedMoves[move.Category] ||= { key: move.Category, moves: [] }
-      categorizedMoves[move.Category].moves.push(move)
+      moves[move.Category] ||= { key: move.Category, moves: [] }
+      moves[move.Category].moves.push(move)
     }
-    // TODO: massage
-    this.moves = categorizedMoves
+    this.moves = moves
   },
 
   computed: {},
+
+  methods: {
+    movesForKey(ck) {
+      return this.moves[ck]?.moves
+    },
+  },
 }
 </script>
