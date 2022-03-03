954275c47976ed60ab254295daae9af34a54dd66
Ben Straub <ben@straub.cc>
2022-03-03 06:55:23 -0800

    Rough in asset tab

---
+++ src/module/vue/components/sf-tabs/sf-assets.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flexcol ironsworn__drop__target" data-drop-type="asset">
+    <transition-group name="slide" tag="div" class="nogrow">
+      <asset
+        v-for="asset in assets"
+        :key="asset._id"
+        :actor="actor"
+        :asset="asset"
+      />
+    </transition-group>
+    <div class="flexrow nogrow" style="text-align: center">
+      <div class="clickable block" @click="openCompendium">
+        <i class="fas fa-atlas" />
+        {{ $t('IRONSWORN.Assets') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    actor: Object,
+  },
+
+  computed: {
+    assets() {
+      return this.actor.items.filter((x) => x.type === 'asset')
+    },
+  },
+
+  methods: {
+    openCompendium() {
+      const pack = game.packs?.get('foundry-ironsworn.starforgedassets')
+      pack?.render(true)
+    },
+  },
+}
+</script>
