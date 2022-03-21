05ea75d0f962460be0d214be6cb67e2ef1e6f28c
Ben Straub <ben@straub.cc>
2022-03-21 06:52:05 -0700

    Ensure libwrapper is enabled

--- src/module/features/dependencies.ts
+++ src/module/features/dependencies.ts
@@ -1,26 +1,28 @@
 export async function maybePromptForDependencies() {
   const dlopen = game.modules.get('dlopen')
   const vueport = game.modules.get('vueport')
+  const libwrapper = game.modules.get('lib-wrapper')
 
   const gm = game.user?.isGM
   const prompt = true // TODO: game.settings.get('archmage', 'dependencyPrompt')
 
   // If the modules don't exist, warn the user.
-  if (!dlopen || !vueport) {
+  if (!dlopen || !vueport || !libwrapper) {
     if (gm) {
       ui.notifications?.error('This system requires the dlopen and vueport modules to be installed.')
     }
   } else {
     // If the modules exist but aren't enabled, prompt the user.
-    if ((dlopen && !dlopen.active) || (vueport && !vueport.active)) {
+    if (!dlopen.active || !vueport.active || !libwrapper.active) {
       if (prompt && gm) {
         Dialog.confirm({
           title: 'Enable dependencies?', // TODO: game.i18n.format('ARCHMAGE.UI.enableDependencies'),
-          content: 'This system requires the VuePort and dlopen modules to be enabled. Would you like to enable them now?', // TODO: game.i18n.format('ARCHMAGE.UI.dependencyContent'),
+          content: 'This system requires the VuePort, dlopen, and libwrapper modules to be enabled. Would you like to enable them now?', // TODO: game.i18n.format('ARCHMAGE.UI.dependencyContent'),
           yes: async () => {
             const moduleSettings = game.settings.get('core', 'moduleConfiguration') as any
             moduleSettings['dlopen'] = true
             moduleSettings['vueport'] = true
+            moduleSettings['lib-wrapper'] = true
             await game.settings.set('core', 'moduleConfiguration', moduleSettings)
             window.location.reload()
           },
