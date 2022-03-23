230053a9453bad0efe18cf5b8a9234bb52b4677e
Ben Straub <ben@straub.cc>
2022-03-23 06:35:06 -0700

    Use vue2-editor for notes tab

--- src/module/vue/components/sf-tabs/sf-notes.vue
+++ src/module/vue/components/sf-tabs/sf-notes.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="flexcol" style="margen: 1rem">
-    <textarea
-      v-model="actor.data.notes"
-      @blur="save"
-    ></textarea>
+    <VueEditor v-model="actor.data.notes" />
   </div>
 </template>
 
-<style lang="less" scoped>
-input,
-textarea {
-  border-color: rgba(0, 0, 0, 0.1);
-  border-radius: 1px;
-  font-family: var(--font-primary);
-  resize: none;
-}
-</style>
-
 <script>
+import { debounce } from 'lodash'
+import { VueEditor } from 'vue2-editor'
+
 export default {
+  components: { VueEditor },
+
   props: {
     actor: Object,
+  },
+
+  watch: {
+    'actor.data.notes'() {
+      this.debouncedSave()
+    },
+  },
+
+  created() {
+    this.debouncedSave = debounce(this.save, 500)
   },
 
   methods: {
