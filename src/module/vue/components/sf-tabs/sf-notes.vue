c346d02c23341349d0bf31733bc4da584301b4f6
Ben Straub <ben@straub.cc>
2022-03-26 06:48:17 -0700

    Cleanup

--- src/module/vue/components/sf-tabs/sf-notes.vue
+++ src/module/vue/components/sf-tabs/sf-notes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flexcol" style="margen: 1rem">
-    <quill-editor v-model="actor.data.notes" :editorToolbar="toolbar" :editorOptions="options" />
+    <quill-editor v-model="actor.data.notes" />
   </div>
 </template>
 
@@ -10,21 +10,6 @@ import { debounce } from 'lodash'
 export default {
   props: {
     actor: Object,
-  },
-
-  data() {
-    return {
-      toolbar: [
-        [{ header: [false, 1, 2, 3, 4] }],
-        ['bold', 'italic', 'underline'],
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        ['blockquote', 'code-block'],
-        ['clean'],
-      ],
-      options: {
-        theme: 'bubble'
-      }
-    }
   },
 
   watch: {
