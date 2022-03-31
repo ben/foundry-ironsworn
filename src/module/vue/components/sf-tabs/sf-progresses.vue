07f57a6cd1f6bb74151e5b508ac6bbe009eadf32
Ben Straub <ben@straub.cc>
2022-03-31 06:57:53 -0700

    Rough in animations

--- src/module/vue/components/sf-tabs/sf-progresses.vue
+++ src/module/vue/components/sf-tabs/sf-progresses.vue
@@ -3,7 +3,7 @@
     <div class="flexcol ironsworn__drop__target" data-drop-type="progress">
       <transition-group name="slide" tag="div" class="nogrow">
         <progress-box
-          v-for="item in progressItems"
+          v-for="item in activeItems"
           :key="item._id"
           :item="item"
           :actor="actor"
@@ -14,7 +14,20 @@
     </div>
 
     <div class="item-row nogrow">
-      <h4 class="clickable text"> <i class="fa fa-caret-right"></i> {{ $t('IRONSWORN.Completed') }}</h4>
+      <h3 class="clickable text" @click="expandCompleted = !expandCompleted">
+        <i :class="completedCaretClass"></i> {{ $t('IRONSWORN.Completed') }}
+      </h3>
+      <transition name="completed-slide" tag="div" class="nogrow">
+        <div v-if="expandCompleted">
+          <progress-box
+            v-for="item in completedItems"
+            :key="item._id"
+            :item="item"
+            :actor="actor"
+            :showStar="true"
+          />
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -27,8 +40,17 @@
   max-height: 83px;
   opacity: 1;
 }
+.completed-slide-enter-active,
+.completed-slide-leave-active {
+  transition: all 0.4s ease-out;
+  overflow: hidden;
+  max-height: 1000px;
+  opacity: 1;
+}
 .slide-enter,
-.slide-leave-to {
+.slide-leave-to,
+.completed-slide-enter,
+.completed-slide-leave-to {
   max-height: 0;
   opacity: 0;
   margin-top: 0;
@@ -46,11 +68,28 @@ export default {
     actor: Object,
   },
 
+  data() {
+    return {
+      expandCompleted: false,
+    }
+  },
+
   computed: {
     progressItems() {
       return this.actor.items
         .filter((x) => x.type === 'progress')
         .filter((x) => x.data.subtype !== 'bond')
+    },
+
+    activeItems() {
+      return this.progressItems.filter((x) => !x.data.completed)
+    },
+    completedItems() {
+      return this.progressItems.filter((x) => x.data.completed)
+    },
+
+    completedCaretClass() {
+      return 'fa fa-caret-' + (this.expandCompleted ? 'down' : 'right')
     },
   },
 }
