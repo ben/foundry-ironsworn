f5fcb84c6c3a5240830575597828bd988d46867a
Ben Straub <ben@straub.cc>
2022-04-03 11:31:42 -0700

    Rough in layout

--- src/module/vue/move-sheet.vue
+++ src/module/vue/move-sheet.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>move</h1>
+  <div class="flexcol">
+    <header class="sheet-header">
+      <document-name :document="item" />
+    </header>
+
+    <!-- Stats -->
+    <div class="flexrow">
+      <label class="checkbox">
+        <input type="checkbox">
+        {{ $t('IRONSWORN.Edge') }}
+      </label>
+    </div>
+
+    <hr class="nogrow">
+
+    <!-- Editors -->
+    <div class="flexrow">
+      <div class="flexcol nogrow">
+        <div class="clickable block tab nogrow selected">Full Text</div>
+        <div class="clickable block tab nogrow">Description</div>
+        <div class="clickable block tab nogrow">Strong Hit</div>
+      </div>
+      <div class="flexcol">
+        <quill-editor v-model="item.data.fulltext" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="less" scoped>
+.tab {
+  padding: 5px;
+  text-align: left;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+}
+</script>
