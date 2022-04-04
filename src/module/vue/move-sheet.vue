a1a3705721474c2e3913ca63247ad1ace609085e
Ben Straub <ben@straub.cc>
2022-04-03 18:55:52 -0700

    Wire up checkboxes

--- src/module/vue/move-sheet.vue
+++ src/module/vue/move-sheet.vue
@@ -5,14 +5,30 @@
     </header>
 
     <!-- Stats -->
-    <div class="flexrow">
+    <div class="flexrow nogrow">
       <label class="checkbox">
-        <input type="checkbox">
+        <input type="checkbox" @change="saveStats" v-model="stats.edge" />
         {{ $t('IRONSWORN.Edge') }}
+      </label>
+      <label class="checkbox">
+        <input type="checkbox" @change="saveStats" v-model="stats.heart" />
+        {{ $t('IRONSWORN.Heart') }}
+      </label>
+      <label class="checkbox">
+        <input type="checkbox" @change="saveStats" v-model="stats.iron" />
+        {{ $t('IRONSWORN.Iron') }}
+      </label>
+      <label class="checkbox">
+        <input type="checkbox" @change="saveStats" v-model="stats.shadow" />
+        {{ $t('IRONSWORN.Shadow') }}
+      </label>
+      <label class="checkbox">
+        <input type="checkbox" @change="saveStats" v-model="stats.wits" />
+        {{ $t('IRONSWORN.Wits') }}
       </label>
     </div>
 
-    <hr class="nogrow">
+    <hr class="nogrow" />
 
     <!-- Editors -->
     <div class="flexrow">
@@ -39,6 +55,34 @@
 export default {
   props: {
     item: Object,
+  },
+
+  watch: {
+    stats: {
+      deep: true,
+      handler(newStats, oldStats) {
+        console.log(newStats, oldStats)
+      },
+    },
+  },
+
+  data() {
+    return {
+      stats: {
+        edge: this.item.data.stats.includes('edge'),
+        heart: this.item.data.stats.includes('heart'),
+        iron: this.item.data.stats.includes('iron'),
+        shadow: this.item.data.stats.includes('shadow'),
+        wits: this.item.data.stats.includes('wits'),
+      },
+    }
+  },
+
+  methods: {
+    saveStats() {
+      const stats = Object.keys(this.stats).filter((k) => this.stats[k])
+      this.$item.update({ data: { stats } })
+    },
   },
 }
 </script>
