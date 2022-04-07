0044fa0528136db7c612d2e82e7e847f1b1ca784
Ben Straub <ben@straub.cc>
2022-04-07 08:14:17 -0700

    Restructure sheet to fit move structure better

---
+++ src/module/vue/components/sfmove-tab.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    @click="$emit('click', property)"
+    :class="[
+      'clickable',
+      'block',
+      'nogrow',
+      'tab',
+      { selected: currentProperty === property },
+    ]"
+  >
+    {{ $t('IRONSWORN.' + titleKey) }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titleKey: String,
+    property: String,
+    currentProperty: String,
+  },
+}
+</script>
--- src/module/vue/sfmove-sheet.vue
+++ src/module/vue/sfmove-sheet.vue
@@ -4,52 +4,62 @@
       <document-name :document="item" />
     </header>
 
-    <!-- Stats -->
-    <div class="flexrow nogrow">
-      <label class="checkbox">
-        <input type="checkbox" @change="saveStats" v-model="stats.edge" />
-        {{ $t('IRONSWORN.Edge') }}
-      </label>
-      <label class="checkbox">
-        <input type="checkbox" @change="saveStats" v-model="stats.heart" />
-        {{ $t('IRONSWORN.Heart') }}
-      </label>
-      <label class="checkbox">
-        <input type="checkbox" @change="saveStats" v-model="stats.iron" />
-        {{ $t('IRONSWORN.Iron') }}
-      </label>
-      <label class="checkbox">
-        <input type="checkbox" @change="saveStats" v-model="stats.shadow" />
-        {{ $t('IRONSWORN.Shadow') }}
-      </label>
-      <label class="checkbox">
-        <input type="checkbox" @change="saveStats" v-model="stats.wits" />
-        {{ $t('IRONSWORN.Wits') }}
-      </label>
-    </div>
-
-    <hr class="nogrow" />
-
     <!-- Editors -->
     <div class="flexrow">
-      <div class="flexcol nogrow">
-        <div
-          v-for="tab in tabs"
-          :key="tab.titleKey"
-          :class="[
-            'clickable',
-            'block',
-            'nogrow',
-            'tab',
-            { selected: currentTab === tab },
-          ]"
-          @click="selectTab(tab)"
-        >
-          {{ $t('IRONSWORN.' + tab.titleKey) }}
-        </div>
+      <!-- Tab selection on left -->
+      <div class="flexcol nogrow" style="white-space: nowrap">
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="FullText"
+          property="Text"
+        />
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="Trigger"
+          property="Trigger.Text"
+        />
+
+        <hr class="nogrow" />
+        (stats)
+
+        <hr class="nogrow" />
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="StrongHit"
+          property="Outcomes.Strong Hit.Text"
+        />
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="StrongHitMatch"
+          property="Outcomes.Strong Hit.With a Match.Text"
+        />
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="WeakHit"
+          property="Outcomes.Weak Hit.Text"
+        />
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="Miss"
+          property="Outcomes.Miss.Text"
+        />
+        <sfmove-tab
+          :currentProperty="currentProperty"
+          @click="switchContent"
+          titleKey="MissMatch"
+          property="Outcomes.Miss.With a Match.Text"
+        />
       </div>
+
+      <!-- Editor on right -->
       <div class="flexcol">
-        <textarea v-model="currentTab.content" @blur="saveText" />
+        <textarea v-model="currentContent" @blur="saveText" />
       </div>
     </div>
   </div>
@@ -63,6 +73,8 @@
 </style>
 
 <script>
+import { get } from 'lodash'
+
 export default {
   props: {
     item: Object,
@@ -78,63 +90,22 @@ export default {
   },
 
   data() {
-    const tabs = [
-      {
-        titleKey: 'Description',
-        property: 'description',
-        content: this.item.data.description,
-      },
-      {
-        titleKey: 'FullText',
-        property: 'fulltext',
-        content: this.item.data.fulltext,
-      },
-      {
-        titleKey: 'StrongHit',
-        property: 'strong',
-        content: this.item.data.strong,
-      },
-      {
-        titleKey: 'StrongHitMatch',
-        property: 'strongmatch',
-        content: this.item.data.strongmatch,
-      },
-      { titleKey: 'WeakHit', property: 'weak', content: this.item.data.weak },
-      { titleKey: 'Miss', property: 'miss', content: this.item.data.miss },
-      {
-        titleKey: 'MissMatch',
-        property: 'missmatch',
-        content: this.item.data.missmatch,
-      },
-    ]
     return {
-      stats: {
-        edge: this.item.data.stats.includes('edge'),
-        heart: this.item.data.stats.includes('heart'),
-        iron: this.item.data.stats.includes('iron'),
-        shadow: this.item.data.stats.includes('shadow'),
-        wits: this.item.data.stats.includes('wits'),
-      },
-
-      tabs,
-      currentTab: tabs[0],
+      currentProperty: 'Text',
+      currentContent: this.item.data.Text,
     }
   },
 
   methods: {
-    saveStats() {
-      const stats = Object.keys(this.stats).filter((k) => this.stats[k])
-      this.$item.update({ data: { stats } })
+    switchContent(prop) {
+      this.currentProperty = prop
+      this.currentContent = get(this.item.data, prop)
     },
 
     saveText() {
-      const { content, property } = this.currentTab
-      this.$item.update({ data: { [property]: content } })
-    },
-
-    selectTab(tab) {
-      this.currentTab = tab
-      this.currentTab.content = this.currentTab.content
+      this.$item.update({
+        data: { [this.currentProperty]: this.currentContent },
+      })
     },
   },
 }
