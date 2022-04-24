232f341a4fe1c2b27cfff3e44dd125dd6c4ce920
Ben Straub <ben@straub.cc>
2022-04-23 17:26:49 -0700

    Render oracle description and table when requested

--- src/module/vue/components/oracletree-node.vue
+++ src/module/vue/components/oracletree-node.vue
@@ -18,13 +18,15 @@
       </h4>
 
       <transition name="slide">
-        <div
+        <with-rolllisteners
+          element="div"
+          :actor="actor"
+          @moveclick="moveclick"
           class="flexcol"
           v-if="descriptionExpanded"
-          style="margin-left: 1rem"
+          v-html="tablePreview"
         >
-          (table preview)
-        </div>
+        </with-rolllisteners>
       </transition>
     </div>
 
@@ -46,6 +48,7 @@
           <oracletree-node
             v-for="child in children"
             :key="child.dfid"
+            :actor="actor"
             :oracle="child"
             :searchQuery="searchQuery"
             :parentMatchesSearch="matchesSearch"
@@ -81,6 +84,7 @@
 <script>
 export default {
   props: {
+    actor: Object,
     oracle: Object,
     searchQuery: String,
     parentMatchesSearch: Boolean,
@@ -122,12 +126,30 @@ export default {
     hidden() {
       if (!this.searchQuery) return false
       return !(
-        (
-          this.matchesSearch || // This matches
-          this.parentMatchesSearch || // Parent matches
-          this.childMatchesSearch
-        )
+        this.matchesSearch || // This matches
+        this.parentMatchesSearch || // Parent matches
+        this.childMatchesSearch
       )
+    },
+
+    tablePreview() {
+      const description = this.oracle.foundryTable.data.description
+      const tableRows = CONFIG.IRONSWORN._.sortBy(
+        this.oracle.foundryTable.data.results.contents.map((x) => ({
+          low: x.data.range[0],
+          high: x.data.range[1],
+          text: x.data.text,
+          selected: false,
+        })),
+        'low'
+      )
+      const markdownTable = [
+        '| Roll | Result |',
+        '| --- | --- |',
+        ...tableRows.map((x) => `| ${x.low}-${x.high} | ${x.text} |`),
+      ].join('\n')
+
+      return this.$enrichMarkdown(description + '\n\n' + markdownTable)
     },
   },
 
@@ -142,6 +164,17 @@ export default {
 
     rollOracle() {
       CONFIG.IRONSWORN.rollAndDisplayOracleResult(this.oracle.foundryTable)
+    },
+
+    moveclick(item) {
+      console.log(item)
+      let actorWithMoves = this.$actor
+      if (this.$actor?.type !== 'character') {
+        actorWithMoves = CONFIG.IRONSWORN.defaultActor()
+      }
+      console.log(actorWithMoves)
+      actorWithMoves?.moveSheet?.render(true)
+      actorWithMoves?.moveSheet?.highlightMove(item)
     },
   },
 }
