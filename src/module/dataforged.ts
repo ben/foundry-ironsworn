7d53a53499c4486eb074fed7dc1120dfb675a61a
Ben Straub <ben@straub.cc>
2022-05-06 06:15:15 -0700

    Hash on demand

--- src/module/dataforged.ts
+++ src/module/dataforged.ts
@@ -149,7 +149,7 @@ function idIsOracleLink(dfid: string): boolean {
   return /^(Starforged|Ironsworn)\/Oracle/.test(dfid)
 }
 
-function renderLinksInStr(text: any, idMap: { [key: string]: string }): any {
+function renderLinksInStr(text: any): string {
   // Strip "Black Medium Right-Pointing Triangle" characters
   text = text.replace('\u23f5', '')
 
@@ -173,15 +173,15 @@ function renderLinksInStr(text: any, idMap: { [key: string]: string }): any {
     if (idIsOracleLink(url)) {
       return `<a class="entity-link oracle-category-link" data-dfid="${url}"><i class="fa fa-caret-right"></i> ${text}</a>`
     }
-    return `@Compendium[foundry-ironsworn.${compendiumKey}.${idMap[url]}]{${text}}`
+    return `@Compendium[foundry-ironsworn.${compendiumKey}.${hash(url)}]{${text}}`
   })
 }
 
-function renderMarkdown(md: string, idMap: { [key: string]: string }, markedFn = marked.parse) {
-  return markedFn(renderLinksInStr(md, idMap))
+function renderMarkdown(md: string, markedFn = marked.parse) {
+  return markedFn(renderLinksInStr(md))
 }
 
-function renderLinksInMove(idMap: { [key: string]: string }, move: IMove) {
+export function renderLinksInMove(move: IMove) {
   const textProperties = [
     'Text',
     'Trigger.Text',
@@ -194,7 +194,7 @@ function renderLinksInMove(idMap: { [key: string]: string }, move: IMove) {
   for (const prop of textProperties) {
     const text = get(move, prop)
     if (!text) continue
-    set(move, prop, renderLinksInStr(text, idMap))
+    set(move, prop, renderLinksInStr(text))
   }
 }
 
@@ -235,7 +235,7 @@ async function processMoves(idMap: { [key: string]: string }) {
   const movesToCreate = [] as (ItemDataConstructorData & Record<string, unknown>)[]
   for (const category of starforged['Move Categories']) {
     for (const move of category.Moves) {
-      renderLinksInMove(idMap, move)
+      renderLinksInMove(move)
       const cleanMove = cleanDollars(move)
       movesToCreate.push({
         _id: idMap[cleanMove['dfid']],
@@ -261,7 +261,7 @@ async function processAssets(idMap: { [key: string]: string }) {
         _id: idMap[asset.$id],
         name: `${assetType.Name} / ${asset.Name}`,
         data: {
-          description: renderMarkdown(assetType.Description, idMap),
+          description: renderMarkdown(assetType.Description),
           fields:
             asset.Inputs?.map((input) => ({
               name: input.Name,
@@ -270,7 +270,7 @@ async function processAssets(idMap: { [key: string]: string }) {
           abilities: (asset.Abilities ?? []).map((ability) => {
             const ret = {
               enabled: ability.Enabled || false,
-              description: renderMarkdown(ability.Text, idMap),
+              description: renderMarkdown(ability.Text),
             } as any
 
             for (const input of ability.Inputs ?? []) {
@@ -308,7 +308,7 @@ async function processOracles(idMap: { [key: string]: string }) {
   // parents prior to their children, and children in order
   async function processOracle(oracle: IOracle) {
     if (oracle.Table) {
-      const description = marked.parseInline(renderLinksInStr(oracle.Description ?? '', idMap))
+      const description = marked.parseInline(renderLinksInStr(oracle.Description ?? ''))
       const maxRoll = max(oracle.Table.map((x) => x.Ceiling || 0)) //oracle.Table && maxBy(oracle.Table, (x) => x.Ceiling)?.Ceiling
       oraclesToCreate.push({
         _id: idMap[oracle.$id],
@@ -331,7 +331,7 @@ async function processOracles(idMap: { [key: string]: string }) {
           return {
             _id: idMap[tableRow.$id ?? ''],
             range: [tableRow.Floor, tableRow.Ceiling],
-            text: tableRow.Result && renderLinksInStr(text, idMap),
+            text: tableRow.Result && renderLinksInStr(text),
           }
         }).filter((x) => x.range[0] !== null),
       })
@@ -358,7 +358,7 @@ async function processEncounters(idMap: { [key: string]: string }) {
   for (const encounter of starforged.Encounters) {
     const description = await renderTemplate('systems/foundry-ironsworn/templates/item/sf-foe.hbs', {
       ...encounter,
-      variantLinks: encounter.Variants.map((x) => renderLinksInStr(`[${x.Name}](${x.$id})`, idMap)),
+      variantLinks: encounter.Variants.map((x) => renderLinksInStr(`[${x.Name}](${x.$id})`)),
     })
 
     encountersToCreate.push({
