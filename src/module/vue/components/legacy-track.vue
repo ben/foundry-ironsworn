9b3765b649e7a69e8dfa89515d7097b435c41d26
rsek <hello@rsek.ca>
2022-05-30 23:16:14 -0700

    clean up legacy track structure

--- src/module/vue/components/legacy-track.vue
+++ src/module/vue/components/legacy-track.vue
@@ -1,48 +1,62 @@
 <template>
-  <section class="flexcol legacy-track">
-    <div class="flexrow">
-      <h4>{{ title }}</h4>
-      <p v-if="overflow" class="legacy-overflow">
-        {{ overflow }}
-      </p>
-      <icon-button v-if="editMode" icon="caret-left" @click="decrease" />
-      <icon-button icon="caret-right" @click="increase" />
-    </div>
+  <fieldset class="legacy-track">
+    <legend class="legacy-track-title">{{ title }}</legend>
 
-    <div class="flexrow track">
-      <div class="flexcol track-box" v-for="(box, i) in boxes" :key="`box${i}`" v-html="box"></div>
-    </div>
+    <section class="legacy-track-controls">
+      <span v-if="overflow" class="legacy-overflow">
+        {{ overflow }}
+      </span>
+      <icon-button v-if="editMode" icon="caret-left" @click="decrease" class="remove-progress" />
+      <icon-button icon="caret-right" @click="increase" class="mark-progress" />
+    </section>
+
+    <progress-track :ticks="ticks"> </progress-track>
 
     <xp-track :max="xpBoxCount" :marked="xpSpent" @click="setXp" />
-  </section>
+  </fieldset>
 </template>
 
 <style lang="less">
+@import '../../../styles/fonts.less';
+
 .legacy-track {
-  h4 {
-    margin: 0.5rem 0;
+  display: grid;
+  grid-template-rows: repeat(3, min-content);
+  grid-template-columns: 1fr max-content;
+  gap: 0 0.5em;
+  grid-auto-flow: dense;
+  grid-template-areas:
+    'title ctrl'
+    'bar bar'
+    'xp xp';
+  .legacy-track-title {
+    grid-area: title;
+    .text-heading();
+    display: contents;
+    padding: 0;
   }
-  .xp {
-    max-height: 40px;
+  .legacy-track-controls {
+    grid-area: ctrl;
+    line-height: 1;
+    display: flex;
+    flex-flow: row nowrap;
+    .legacy-overflow {
+      padding: 1px;
+      margin-right: 10px;
+      flex-grow: 0;
+    }
   }
-  .legacy-overflow {
-    padding: 1px;
-    margin-right: 10px;
-    flex-grow: 0;
+  .progress-track {
+    grid-area: bar;
+  }
+  .xp-track {
+    margin-top: 0.5em;
+    grid-area: xp;
   }
 }
 </style>
 
 <script>
-function ticksSvg(ticks) {
-  let ret = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">'
-  if (ticks > 0) ret += '<line x1="23" y1="23" x2="77" y2="77" />'
-  if (ticks > 1) ret += '<line x1="77" y1="23" x2="23" y2="77" />'
-  if (ticks > 2) ret += '<line x1="15" y1="50" x2="85" y2="50" />'
-  if (ticks > 3) ret += '<line x1="50" y1="15" x2="50" y2="85" />'
-  return ret + '</svg>'
-}
-
 export default {
   props: {
     actor: Object,
@@ -83,13 +97,16 @@ export default {
       }
     },
     boxes() {
-      const ret = []
-      let remainingTicks = this.ticks % 40
-      for (let i = 0; i < 10; i++) {
-        ret.push(ticksSvg(remainingTicks))
-        remainingTicks -= 4
+      const boxesPerTrack = 10
+      const ticksPerBox = 4
+      let remainingTicks = this.ticks.valueOf()
+      let result = []
+      for (let i = 0; i < boxesPerTrack; i++) {
+        const ticksToPush = Math.min(ticksPerBox, remainingTicks.valueOf())
+        remainingTicks -= ticksToPush
+        result.push(ticksToPush)
       }
-      return ret
+      return result
     },
   },
 
--- src/module/vue/components/xp-track.vue
+++ src/module/vue/components/xp-track.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="flexrow xp nogrow">
-    <div
-      class="clickable block xp"
+  <section class="xp-track">
+    <button
+      class="clickable block xp-box"
+      type="button"
       v-for="(box, i) in computedBoxes"
       :key="box.key"
       :class="box.classes"
+      :disabled="box.classes.disabled"
       @mouseover="hovered = i"
       @mouseleave="hovered = -1"
       @click="click(i)"
     />
-  </div>
+  </section>
 </template>
+
+<style lang="less">
+.xp-track {
+  display: flex;
+  flex-grow: 0;
+  flex-direction: row;
+  // flex-basis: 140px;
+  align-items: center;
+  justify-content: space-around;
+  max-height: 40px;
+  gap: 3px;
+  .xp-box {
+    height: 15px;
+    flex-basis: 15px;
+    border: 1px solid;
+    margin: 0;
+    &:disabled {
+      opacity: 0.25;
+      pointer-events: none;
+    }
+  }
+}
+</style>
 
 <script>
 export default {
@@ -18,29 +43,42 @@ export default {
     max: Number,
     marked: Number,
   },
-
   data() {
     return {
       hovered: -1,
     }
   },
-
   computed: {
     computedBoxes() {
-      const ret = []
-      for (let i = 0; i < this.max; i++) {
-        ret.push({
-          key: `box${i}`,
-          classes: {
-            hover: this.hovered >= i,
-            selected: this.marked >= i + 1,
-          },
-        })
+      const boxesPerTrack = 10
+      const xpPerBox = 2
+      const xpBoxes = boxesPerTrack * xpPerBox
+      let boxes = []
+      for (let i = 0; i < xpBoxes; i++) {
+        if (i < this.max) {
+          boxes.push({
+            key: `box${i}`,
+            classes: {
+              hover: this.hovered >= i,
+              selected: this.marked >= i + 1,
+              disabled: false,
+            },
+          })
+        } else {
+          boxes.push({
+            key: `box${i}`,
+            classes: {
+              hover: false,
+              selected: false,
+              disabled: true,
+            },
+          })
+        }
       }
-      return ret
+      // console.log('computedBoxes', boxes)
+      return boxes
     },
   },
-
   methods: {
     click(i) {
       if (i === 0 && this.marked === 1) {
