cc20bf413fa33e46f501bd7e70cca6a3d7a9abb8
rsek <hello@rsek.ca>
2022-05-30 23:21:54 -0700

    add styles, div -> article

--- src/module/vue/components/sf-character-sheet-tabs/sf-legacies.vue
+++ src/module/vue/components/sf-character-sheet-tabs/sf-legacies.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
+  <article class="page-legacies">
     <legacy-track propKey="quests" :title="$t('IRONSWORN.Quests')" :actor="actor" />
     <legacy-track propKey="bonds" :title="$t('IRONSWORN.Bonds')" :actor="actor" />
     <legacy-track propKey="discoveries" :title="$t('IRONSWORN.Discoveries')" :actor="actor" />
 
     <hr class="nogrow" v-if="starredProgresses.length > 0" />
     <progress-tracker v-for="item in starredProgresses" :key="item._id" :item="item" :actor="actor" />
-  </div>
+  </article>
 </template>
+
+<style lang="less">
+.page-legacies {
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1em;
+}
+</style>
 
 <script>
 export default {
