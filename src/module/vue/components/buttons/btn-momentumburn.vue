3f260a2086931e9eb3d337ae0b6ef4aa2fe5c454
rsek <hello@rsek.ca>
2022-06-14 03:17:47 -0700

    tweak icon to better resemble pre FAv5

---
+++ src/module/vue/components/buttons/btn-momentumburn.vue
@@ -0,0 +1,18 @@
+<template>
+  <btn-faicon icon="flame" class="burn-momentum">
+    <slot></slot>
+  </btn-faicon>
+</template>
+
+<script>
+export default {
+  props: {
+    actor: Object,
+  },
+  methods: {
+    burnMomentum() {
+      this.$actor.burnMomentum()
+    }
+  }
+}
+</script>
