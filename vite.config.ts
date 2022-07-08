3b2d571f84ea0a6473e3f0ef276acf47b87f88a9
Ben Straub <ben@straub.cc>
2022-07-08 12:03:43 -0700

    Vite config

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import type { UserConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const config: UserConfig = {
+  plugins: [vue()],
+  publicDir: 'system',
+  base: '/systems/foundry-ironsworn/',
+  server: {
+    port: 8080,
+    proxy: {
+      '^(?!/systems/foundry-ironsworn)': 'http://localhost:30000/',
+      '/socket.io': {
+        target: 'ws://localhost:30000',
+        ws: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    lib: {
+      name: 'ironsworn',
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: () => 'ironsworn.js',
+    },
+  },
+}
+
+export default config
