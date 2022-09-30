525ffb190ebf141cac705b71a4a5792283c928b2
rsek <r.sekouri@gmail.com>
2022-09-30 09:37:30 -0700

    revert to main

--- src/module/vue/components/xp-box.vue
+++ src/module/vue/components/xp-box.vue
@@ -1,20 +1,18 @@
 <template>
-  <div
-    class="xp-box-classic clickable block"
-    @click="$emit('click')"
-    :class="classes"
-    data-resource="xp"
-  >
+  <div @click="$emit('click')" :class="classes" data-resource="xp">
     <slot />
   </div>
 </template>
-<style lang="less"></style>
+
 <script setup lang="ts">
 import { computed } from '@vue/runtime-core'
 const props = defineProps<{ value: number; current: number }>()
 
 const classes = computed(() => {
   return {
+    clickable: true,
+    block: true,
+    xp: true,
     selected: selected.value,
   }
 })
