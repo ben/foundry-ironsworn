df8bf3080130ae96b8d2e3e3b12edeaf6e5a2e27
rsek <hello@rsek.ca>
2022-09-30 00:38:46 -0700

    restore progress-track.vue

---
+++ src/module/vue/components/progress/progress-track.vue
@@ -0,0 +1,34 @@
+<!-- old component, gradually being replaced by 'track' -->
+<template>
+  <div class="flexrow track">
+    <div class="flexcol track-box" v-for="(box, i) in boxes" :key="'box' + i">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        v-html="box"
+      ></svg>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from '@vue/runtime-core'
+function ticksSvg(ticks: number) {
+  let ret = ''
+  if (ticks > 0) ret += '<line x1="23" y1="23" x2="77" y2="77" />'
+  if (ticks > 1) ret += '<line x1="77" y1="23" x2="23" y2="77" />'
+  if (ticks > 2) ret += '<line x1="15" y1="50" x2="85" y2="50" />'
+  if (ticks > 3) ret += '<line x1="50" y1="15" x2="50" y2="85" />'
+  return ret + ''
+}
+const props = defineProps<{ ticks: number }>()
+const boxes = computed(() => {
+  const ret = [] as string[]
+  let remainingTicks = props.ticks
+  for (let i = 0; i < 10; i++) {
+    ret.push(ticksSvg(remainingTicks))
+    remainingTicks -= 4
+  }
+  return ret
+})
+</script>
