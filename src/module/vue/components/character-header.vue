b7837440c275993d14f6de1af125211fc7fb2e99
rsek <hello@rsek.ca>
2022-09-30 01:05:07 -0700

    revert to main

--- src/module/vue/components/character-header.vue
+++ src/module/vue/components/character-header.vue
@@ -3,10 +3,8 @@
     <section class="flexrow xp" style="gap: 5px">
       <h4 class="nogrow" style="margin: 0">{{ $t('IRONSWORN.XP') }}</h4>
       <div class="flexrow">
-        <XpBoxClassic :key="0" :current="-1" :value="0" @click="setXp(0)">
-          ×
-        </XpBoxClassic>
-        <XpBoxClassic
+        <xp-box :key="0" :current="-1" :value="0" @click="setXp(0)"> × </xp-box>
+        <xp-box
           v-for="n in xpArray"
           :key="n"
           :value="n"
@@ -22,13 +20,9 @@
 import SheetHeaderBasic from '../sheet-header-basic.vue'
 import { Ref, inject } from 'vue'
 import { $ActorKey } from '../provisions'
-import DocumentImg from './document-img.vue'
-import DocumentName from './document-name.vue'
-import XpBoxClassic from './xp/xp-box-classic.vue'
-
+import XpBox from './xp-box.vue'
 const actor = inject('actor') as Ref
 const xpArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 const $actor = inject($ActorKey)
 function setXp(n) {
   $actor?.update({ data: { xp: n } })
