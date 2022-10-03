fa38854775f68152f6d8dd2f96455655b53ee2a5
Ben Straub <ben@straub.cc>
2022-10-03 06:18:36 -0700

    Use "requirement" for asset description field

--- src/module/dataforged/import.ts
+++ src/module/dataforged/import.ts
@@ -169,7 +169,7 @@ function assetsForTypes(types: IAssetType[]) {
       }
 
       const data = {
-        description: renderMarkdown(assetType.Description),
+        description: renderMarkdown(asset.Requirement ?? ''),
         category: assetType.Name,
         color: assetType.Display.Color ?? '',
         fields,
