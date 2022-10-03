0573597e781753c3fb07ae5b0cdb56ffc13d521d
Ben Straub <ben@straub.cc>
2022-10-03 06:25:27 -0700

    Description is too loaded, use requirement instead

--- src/module/dataforged/import.ts
+++ src/module/dataforged/import.ts
@@ -169,7 +169,7 @@ function assetsForTypes(types: IAssetType[]) {
       }
 
       const data = {
-        description: renderMarkdown(asset.Requirement ?? ''),
+        requirement: renderMarkdown(asset.Requirement ?? ''),
         category: assetType.Name,
         color: assetType.Display.Color ?? '',
         fields,
