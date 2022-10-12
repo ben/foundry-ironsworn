a488a28f8d3a2efab22198cc70ed88df8315c06b
Ben Straub <ben@straub.cc>
2022-10-12 06:35:26 -0700

    Progress boxes with ticks

--- src/module/journal/progress-page.ts
+++ src/module/journal/progress-page.ts
@@ -1,3 +1,6 @@
+import { fill, range } from 'lodash'
+import { NumericRank } from '../dataforged'
+
 declare global {
   class JournalPageSheet extends DocumentSheet {}
 }
@@ -13,6 +16,47 @@ export class JournalProgressPageSheet extends JournalPageSheet {
     return `systems/foundry-ironsworn/templates/journal/progress-page-${
       this.isEditable ? 'edit' : 'view'
     }.hbs`
+  }
+
+  getData(options?: Partial<DocumentSheetOptions> | undefined): any {
+    const data = super.getData(options) as any
+
+    // Foundry doesn't use the template data here, we have to supply our own
+    data.data.system.ticks ??= 0
+    data.data.system.rank ??= NumericRank.troublesome
+
+    // Compute some basic information
+    const boxes = range(10).map((_) => ({
+      ticks: 0,
+      lineTransforms: [] as string[],
+    }))
+    const ticksRemainder = data.data.system.ticks % 4
+    data.filledBoxes = Math.floor(data.data.system.ticks / 4)
+
+    fill(boxes, { ticks: 4, lineTransforms: [] }, 0, data.filledBoxes)
+    boxes[data.filledBoxes] = { ticks: ticksRemainder, lineTransforms: [] }
+    console.log(boxes)
+
+    // List of line transforms
+    const transforms = [
+      'rotate(-45, 50, 50)',
+      'rotate(45, 50, 50)',
+      'rotate(-90, 50, 50)',
+      '',
+    ]
+    for (let i = 0; i < boxes.length; i++) {
+      const box = boxes[i]
+
+      if (box.ticks > 0) box.lineTransforms.push(transforms[0])
+      if (box.ticks > 1) box.lineTransforms.push(transforms[1])
+      if (box.ticks > 2) box.lineTransforms.push(transforms[2])
+      if (box.ticks > 3) box.lineTransforms.push(transforms[3])
+    }
+    data.boxes = boxes
+
+    // Compute the svg marks for progress
+    console.log(data)
+    return data
   }
 }
 
