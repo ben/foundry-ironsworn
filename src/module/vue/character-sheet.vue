f5158bcf963c69c7f884d53e794603067edf1344
rsek <hello@rsek.ca>
2022-10-13 21:29:33 -0700

    fix bad merge

--- src/module/vue/character-sheet.vue
+++ src/module/vue/character-sheet.vue
@@ -38,61 +38,12 @@
     </div>
 
     <!-- Stats on right -->
-    <<<<<<< HEAD
-    <div class="flexcol right-margin" data-tooltip-direction="UP">
-      <div class="flexrow nogrow" style="flex-wrap: nowrap">
-        <!-- TODO: restyle as h4-like -->
-        <btn-rollstat
-          class="nogrow vertical text"
-          attr="health"
-          :statLabel="$t('IRONSWORN.Health')"
-        >
-          {{ $t('IRONSWORN.Health') }}
-        </btn-rollstat>
-        <div class="flexcol stack health">
-          <stack stat="health" :top="5" :bottom="0"></stack>
-        </div>
-      </div>
 
-      <hr class="nogrow" />
-
-      <div class="flexrow nogrow" style="flex-wrap: nowrap">
-        <!-- TODO: restyle as h4-like -->
-        <btn-rollstat
-          class="nogrow vertical text"
-          attr="spirit"
-          :statLabel="$t('IRONSWORN.Spirit')"
-        >
-          {{ $t('IRONSWORN.Spirit') }}
-        </btn-rollstat>
-        <div class="flexcol stack spirit">
-          <stack stat="spirit" :top="5" :bottom="0"></stack>
-        </div>
-      </div>
-
-      <hr class="nogrow" />
-
-      <div class="flexrow nogrow" style="flex-wrap: nowrap">
-        <!-- TODO: restyle as h4-like -->
-        <btn-rollstat
-          class="nogrow vertical text"
-          attr="supply"
-          :statLabel="$t('IRONSWORN.Supply')"
-        >
-          {{ $t('IRONSWORN.Supply') }}
-        </btn-rollstat>
-        <div class="flexcol stack supply">
-          <stack stat="supply" :top="5" :bottom="0"></stack>
-        </div>
-      </div>
-    </div>
-    =======
     <PcConditionMeters
-      class="flexcol margin-right"
+      class="flexcol right-margin"
       data-tooltip-direction="UP"
       labelPosition="left"
     />
-    >>>>>>> main
   </SheetBasic>
 </template>
 
