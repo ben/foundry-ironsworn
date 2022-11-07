87d85390dcd9fa577b84fc218ff6fa45750e514c
Ben Straub <ben@straub.cc>
2022-11-06 20:46:08 -0800

    Update attr-box.vue

--- src/module/vue/components/attr-box.vue
+++ src/module/vue/components/attr-box.vue
@@ -9,7 +9,7 @@
       <div class="clickable text" v-if="editMode" @click="decrement">
         &minus;
       </div>
-      <h4>{{ actor.data[attr] }}</h4>
+      <h4>{{ actor.system[attr] }}</h4>
       <div class="clickable text" v-if="editMode" @click="increment">
         &plus;
       </div>
@@ -68,19 +68,15 @@ function click() {
   let attrName = game.i18n.localize('IRONSWORN.' + capitalize(props.attr))
   if (attrName.startsWith('IRONSWORN.')) attrName = props.attr
   const name = `${attrName} (${$actor?.name})`
-  IronswornPrerollDialog.showForStat(
-    name,
-    $actor?.data.data[props.attr],
-    $actor
-  )
+  IronswornPrerollDialog.showForStat(name, $actor?.system[props.attr], $actor)
 }
 
 function increment() {
-  const value = parseInt(actor.value.data[props.attr]) + 1
+  const value = parseInt(actor.value.system[props.attr]) + 1
   $actor?.update({ data: { [props.attr]: value } })
 }
 function decrement() {
-  const value = parseInt(actor.value.data[props.attr]) - 1
+  const value = parseInt(actor.value.system[props.attr]) - 1
   $actor?.update({ data: { [props.attr]: value } })
 }
 </script>
