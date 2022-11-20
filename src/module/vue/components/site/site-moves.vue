d38065e1ae97e82747c1ab5473a297ffd548411f
Ben Straub <ben@straub.cc>
2022-11-20 15:35:09 -0800

    Use standard buttons

--- src/module/vue/components/site/site-moves.vue
+++ src/module/vue/components/site/site-moves.vue
@@ -1,42 +1,53 @@
 <template>
   <div class="boxgroup moves nogrow" style="margin-bottom: 1em">
-    <div class="flexrow boxrow">
-      <SiteMovebox movename="Delve the Depths" />
-      <!-- TODO: double check styling here -->
-      <button
-        type="button"
-        class="box flexrow clickable block"
+    <div class="flexrow boxrow" style="justify-items: stretch">
+      <BtnRollmove :move="moves.delveTheDepths" class="box juicy text block">
+        {{ moves.delveTheDepths?.displayName }}
+      </BtnRollmove>
+
+      <BtnIsicon
+        icon="d10-tilt"
+        class="box juicy text block"
         :class="{ disabled: !hasThemeAndDomain }"
         @click="randomFeature"
       >
-        <h4>
-          {{ $t('IRONSWORN.Feature') }}
-        </h4>
-      </button>
+        {{ $t('IRONSWORN.Feature') }}
+      </BtnIsicon>
 
-      <SiteMovebox movename="Reveal a Danger" :disabled="!hasThemeAndDomain" />
+      <BtnRollmove
+        :move="moves.revealADanger"
+        class="box juicy text block"
+        :disabled="!hasThemeAndDomain"
+      >
+        {{ moves.revealADanger?.displayName }}
+      </BtnRollmove>
     </div>
     <div class="flexrow boxrow">
       <!-- This one is really just an oracle roll with some description -->
-      <SiteMovebox movename="Find an Opportunity" />
-      <button
-        type="button"
-        class="box flexrow clickable block"
+      <BtnRollmove :move="moves.findAnOpportunity" class="box juicy text block">
+        {{ moves.findAnOpportunity?.displayName }}
+      </BtnRollmove>
+
+      <BtnIsicon
+        icon="d10-tilt"
+        class="box juicy text block"
         @click="locateObjective"
       >
-        <h4>
-          {{ $t('IRONSWORN.MoveContents.Locate Your Objective.title') }}
-        </h4>
-      </button>
-      <SiteMovebox movename="Escape the Depths" />
+        {{ $t('IRONSWORN.MoveContents.Locate Your Objective.title') }}
+      </BtnIsicon>
+
+      <BtnRollmove :move="moves.escapeTheDepths" class="box juicy text block">
+        {{ moves.escapeTheDepths?.displayName }}
+      </BtnRollmove>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, inject } from 'vue'
+import { computed, inject, reactive } from 'vue'
 import { SiteDataPropertiesData } from '../../../actor/actortypes'
 import { createIronswornChatRoll } from '../../../chat/chatrollhelpers'
+import { createIronswornMoveTree, Move } from '../../../features/custommoves'
 import { moveDataByName } from '../../../helpers/data'
 import {
   DelveThemeDataSourceData,
@@ -45,7 +56,8 @@ import {
 import { TableRow, OracleRollMessage } from '../../../rolls'
 import { $ActorKey, ActorKey } from '../../provisions'
 
-import SiteMovebox from './site-movebox.vue'
+import BtnRollmove from '../buttons/btn-rollmove.vue'
+import BtnIsicon from '../buttons/btn-isicon.vue'
 
 const site = inject(ActorKey)
 const $site = inject($ActorKey)
@@ -61,11 +73,34 @@ const hasThemeAndDomain = computed(() => {
   return !!(theme.value && domain.value)
 })
 
+// Construct some moves to use with the new pipeline
+const moves = reactive<{ [k: string]: Move }>({})
+Promise.resolve().then(async () => {
+  const moveTree = await createIronswornMoveTree()
+  const delveMoves = moveTree.find(
+    (x) => x.dataforgedCategory?.$id === 'Ironsworn/Moves/Delve'
+  )
+  if (!delveMoves) return
+
+  const movesToFetch = {
+    delveTheDepths: 'Ironsworn/Moves/Delve/Delve_the_Depths',
+    revealADanger: 'Ironsworn/Moves/Delve/Reveal_a_Danger',
+    findAnOpportunity: 'Ironsworn/Moves/Delve/Find_an_Opportunity',
+    escapeTheDepths: 'Ironsworn/Moves/Delve/Escape_the_Depths',
+  }
+
+  for (const k of Object.keys(movesToFetch)) {
+    moves[k] = delveMoves.moves.find(
+      (x) => x.dataforgedMove?.$id === movesToFetch[k]
+    )!
+  }
+})
+
 async function randomFeature() {
   if (!hasThemeAndDomain.value) return
 
-  const themeData = theme.value?.system as DelveThemeDataSourceData
-  const domainData = domain.value?.system as DelveThemeDataSourceData
+  const themeData = (theme.value as any)?.system as DelveThemeDataSourceData
+  const domainData = (domain.value as any)?.system as DelveThemeDataSourceData
   const convertToRow = (f: FeatureOrDanger): TableRow => {
     const { low, high, description } = f
     return {
