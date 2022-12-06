ed5e6151133943fcf6a1628a90ee1af996b53470
rsek <hello@rsek.ca>
2022-12-05 20:42:34 -0800

    add component validation function

--- src/module/vue/components/tabs/tab-helpers.ts
+++ src/module/vue/components/tabs/tab-helpers.ts
@@ -1,7 +1,7 @@
 // adapted from https://github.com/jakedohm/vue-accessible-tabs
 // more about the recommendations for this design pattern, for later reference: https://www.w3.org/WAI/ARIA/apg/example-index/tabs/tabs-manual.html
 
-import { InjectionKey } from 'vue'
+import { inject, InjectionKey } from 'vue'
 
 export type TabKey = string | number
 export type TabActivationMode = 'auto' | 'manual'
@@ -31,3 +31,41 @@ export const SetActivePanelRefKey = Symbol(
 export const FocusActivePanelKey = Symbol(
   'focusActivePanel'
 ) as InjectionKey<FocusActivePanel>
+
+/**
+ * Validates descendant Tab and TabPanel components.
+ * @param role The component type to validate.
+ * @param element The element of the mounted component.
+ */
+export function validateTabSetRole(
+  element: HTMLElement,
+  role: 'TabPanel' | 'Tab'
+) {
+  const tabState = inject(TabStateKey) as TabState
+
+  const tabElements = element.querySelectorAll(
+    `[data-tab-set="${tabState.tabSetId}"][role=${role.toLowerCase()}]`
+  )
+  if (!tabElements ?? tabElements?.length !== 0) {
+    throw Error(
+      `TabList for ${tabState.tabSetId} contains no ${role} descendants.`
+    )
+  }
+  const tabElementKeys = new Set()
+  tabElements.forEach((tabElement) => {
+    const tabElementKey = (tabElement as HTMLElement).dataset.tabKey
+    if (!tabElementKey) {
+      throw Error(`${role}'s key is invalid: ${tabElementKey?.toString()} `)
+    }
+    tabElementKeys.add(tabElementKey)
+  })
+  tabState.tabKeys.forEach((tabComponentKey) => {
+    if (!tabElementKeys.has(tabComponentKey.toString())) {
+      throw Error(
+        `TabSet includes a ${role} keyed with ${JSON.stringify(
+          tabComponentKey
+        )}, but this isn't included in the TabSet's tabKeys prop.`
+      )
+    }
+  })
+}
