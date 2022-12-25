477b45a05ef0465065b6099d3914464b8ba7712e
Ben Straub <ben@straub.cc>
2022-12-25 10:08:19 -0800

    Rough in a standalone oracles window

---
+++ src/module/applications/oracle-window.ts
@@ -0,0 +1,22 @@
+import { VueSheetRenderHelperOptions } from '../vue/vue-render-helper'
+import { VueAppMixin } from '../vue/vueapp'
+import OracleWindowComponent from '../vue/oracle-window.vue'
+
+export class OracleWindow extends VueAppMixin(Application) {
+  static get defaultOptions(): ApplicationOptions {
+    return mergeObject(super.defaultOptions, {
+      title: game.i18n.localize('IRONSWORN.Oracles'),
+      template: 'systems/foundry-ironsworn/templates/oracle-window.hbs',
+      id: 'oracles',
+      resizable: true,
+      width: 350,
+      height: 400,
+    })
+  }
+
+  get renderHelperOptions(): Partial<VueSheetRenderHelperOptions> {
+    return {
+      components: { Oracles: OracleWindowComponent },
+    }
+  }
+}
---
+++ src/module/vue/oracle-window.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flexcol">
+    <!-- TODO:  -->
+    <p class="nogrow">(Dropdown to select toolset)</p>
+
+    <Suspense>
+      <SfMovesheetoracles toolset="starforged" />
+    </Suspense>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SfMovesheetoracles from './components/sf-movesheetoracles.vue'
+</script>
