53fd0bf485a3f09735d0c8df0de969f08f7c9d46
rsek <r.sekouri@gmail.com>
2023-01-09 14:59:56 -0800

    harmonize config across PRs

--- stylelint.config.js
+++ stylelint.config.js
@@ -40,7 +40,7 @@ const CONFIG = {
           ignoreProperties: ['/^\\$/'],
           ignoreValues: ['/\\$/', 'transparent', 'currentcolor', '0'],
         },
-        'custom-property-pattern': [/ironsworn-/],
+        'custom-property-pattern': [/(ironsworn|font|fa|color)-/],
         'max-line-length': [120, { ignore: ['comments'] }],
         // *theoretically* this would be good to use, but i don't have the patience to do it right now
         'no-descending-specificity': null,
