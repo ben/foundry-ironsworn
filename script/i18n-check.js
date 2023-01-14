d70c7a6ef3940d799570fedc8a5eb4c9ed666d7c
rsek <r.sekouri@gmail.com>
2023-01-14 00:11:21 -0800

    simple script to check i18n keys

--- script/i18n-check.js
+++ script/i18n-check.js
@@ -84,4 +84,44 @@ function setProperty(object, key, value) {
   return changed
 }
 
-console.log()
+const masterLocale = 'en'
+
+const locales = ['en', 'de', 'es', 'fr', 'pl']
+
+/**
+ * @type {Record<string,Set<string>>}
+ */
+const localeKeys = {}
+
+locales.forEach(
+  (locale) =>
+    (localeKeys[locale] = new Set(
+      Object.keys(flattenObject(require(`../system/lang/${locale}.json`)))
+    ))
+)
+
+_.forEach(localeKeys, (keys, locale) => {
+  if (locale !== masterLocale) {
+    const masterKeys = localeKeys[masterLocale]
+    const extraKeys = difference(keys, masterKeys)
+    console.log(`Extra keys in ${locale}.json:\n`, extraKeys)
+    // const missingKeys = difference(masterKeys, keys)
+    // console.log(`Missing keys in ${locale}.json:\n`, missingKeys)
+  }
+})
+
+/**
+ *
+ * @param {Set<any>} setA
+ * @param {Set<any>} setB
+ * @returns {Set<any>}
+ */
+function difference(setA, setB) {
+  const _difference = new Set(setA)
+  for (const elem of setB) {
+    _difference.delete(elem)
+  }
+  return _difference
+}
+
+// TODO: check that the variable name is consistent from string to string
