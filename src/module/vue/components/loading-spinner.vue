9c89bb7c885af89712ab0d1d3781d811ce1b1374
Ben Straub <ben@straub.cc>
2023-01-24 08:28:51 -0800

    Switch to vue-loading-overlay

--- src/module/vue/components/loading-spinner.vue
+++ src/module/vue/components/loading-spinner.vue
@@ -1,13 +1,8 @@
 <template>
-  <GridLoader
-    :loading="true"
-    color="#ccc"
-    style="margin: auto; max-width: 57px"
-  />
+  <Loading :active="true" loader="bars" :isFullPage="false" />
 </template>
 
 <script lang="ts" setup>
-import * as Loaders from 'vue-spinner/src/index'
-
-const GridLoader = Loaders.GridLoader
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
 </script>
