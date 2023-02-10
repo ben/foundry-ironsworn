04008800440fd7f88bbef35c37a53e44d7e32121
Ben Straub <ben@straub.cc>
2023-02-10 06:29:24 -0800

    Progress sheet

--- src/module/item/progress/progresssheet-v2.ts
+++ src/module/item/progress/progresssheet-v2.ts
@@ -5,14 +5,13 @@ import { VueItemSheet } from '../../vue/vueitemsheet'
 export class ProgressSheetV2 extends VueItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      template: 'systems/foundry-ironsworn/templates/item/progressv2.hbs',
       height: 550,
     })
   }
 
   get renderHelperOptions(): Partial<VueSheetRenderHelperOptions> {
     return {
-      components: { 'progress-sheet': progressSheetVue },
+      rootComponent: progressSheetVue,
     }
   }
 
