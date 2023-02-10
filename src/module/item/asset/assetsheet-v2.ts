fcc20367db68982e9963c455bd6f32c4524d7625
Ben Straub <ben@straub.cc>
2023-02-10 09:57:07 -0800

    Asset sheet

--- src/module/item/asset/assetsheet-v2.ts
+++ src/module/item/asset/assetsheet-v2.ts
@@ -5,14 +5,13 @@ import { VueItemSheet } from '../../vue/vueitemsheet'
 export class AssetSheetV2 extends VueItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      template: 'systems/foundry-ironsworn/templates/item/assetv2.hbs',
       width: 450,
     })
   }
 
   get renderHelperOptions(): Partial<VueSheetRenderHelperOptions> {
     return {
-      components: { 'asset-sheet': assetSheetVue },
+      rootComponent: assetSheetVue,
     }
   }
 }
