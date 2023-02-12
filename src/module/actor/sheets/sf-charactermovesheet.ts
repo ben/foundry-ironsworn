a7e653fa642f19323f87200905d9c7c31874c482
Ben Straub <ben@straub.cc>
2023-02-12 12:50:15 -0800

    Remove the old class and file

--- src/module/actor/sheets/sf-charactermovesheet.ts
+++ src/module/actor/sheets/sf-charactermovesheet.ts
@@ -1,6 +1,5 @@
 import CharacterMoveSheet from '../../vue/sf-charactermovesheet.vue'
 import { IronswornActor } from '../actor'
-import { VueSheetRenderHelperOptions } from '../../vue/vue-render-helper'
 import { App } from 'vue'
 import { $ActorKey } from '../../vue/provisions'
 import { VueAppMixin } from '../../vue/vueapp.js'
