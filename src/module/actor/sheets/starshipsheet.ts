ccd69ea35acbba0705ee49329434d323264c190c
Ben Straub <ben@straub.cc>
2023-02-12 12:38:56 -0800

    Starship sheet

--- src/module/actor/sheets/starshipsheet.ts
+++ src/module/actor/sheets/starshipsheet.ts
@@ -1,5 +1,4 @@
 import starshipSheetVue from '../../vue/starship-sheet.vue'
-import { VueSheetRenderHelperOptions } from '../../vue/vue-render-helper'
 import { VueActorSheet } from '../../vue/vueactorsheet'
 
 export class StarshipSheet extends VueActorSheet {
@@ -7,12 +6,7 @@ export class StarshipSheet extends VueActorSheet {
     return mergeObject(super.defaultOptions, {
       width: 500,
       height: 500,
-    })
-  }
-
-  get renderHelperOptions(): Partial<VueSheetRenderHelperOptions> {
-    return {
       rootComponent: starshipSheetVue,
-    }
+    })
   }
 }
