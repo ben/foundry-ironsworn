1c262d453a3e93a315d024ee6587f7c93a282306
Ben Straub <ben@straub.cc>
2023-02-12 12:47:31 -0800

    Bondset sheet

--- src/module/item/bondset/bondsetsheet-v2.ts
+++ src/module/item/bondset/bondsetsheet-v2.ts
@@ -1,17 +1,11 @@
 import bondsetSheetVue from '../../vue/bondset-sheet.vue'
-import { VueSheetRenderHelperOptions } from '../../vue/vue-render-helper'
 import { VueItemSheet } from '../../vue/vueitemsheet'
 
 export class BondsetSheetV2 extends VueItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       width: 800,
-    })
-  }
-
-  get renderHelperOptions(): Partial<VueSheetRenderHelperOptions> {
-    return {
       rootComponent: bondsetSheetVue,
-    }
+    })
   }
 }
