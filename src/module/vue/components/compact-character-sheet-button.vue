1a9e83e5b71056f5c80cf92194852262e88632c9
Ben Straub <ben@straub.cc>
2023-02-13 08:49:47 -0800

    Hover styles

--- src/module/vue/components/compact-character-sheet-button.vue
+++ src/module/vue/components/compact-character-sheet-button.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="box flexcol block" style="height: 100%">
-    <h4 :class="$style.statTitle">
-      {{ $t(`IRONSWORN.${$capitalize(propKey)}`) }}
-    </h4>
-    <h4>{{ value }}</h4>
+  <div
+    class="box flexcol block isiconbg-d10-tilt"
+    :class="$style.wrapper"
+  >
+    <h4 :class="$style.statTitle" :data-tooltip="tooltip">{{ i18nStat }}</h4>
+    <h4 :data-tooltip="tooltip">{{ value }}</h4>
     <div class="flexrow clickable" style="flex: 1; justify-content: center">
       <IronBtn icon="fa:subtract" @click="increment(-1)" />
       <IronBtn
@@ -17,6 +18,34 @@
 </template>
 
 <style lang="less" module>
+@import (reference) '../../../styles/mixins.less';
+
+.wrapper {
+  --ironsworn-color-text-stroke: var(--ironsworn-color-bg);
+  cursor: pointer;
+  .interactiveMixin();
+
+  &::before {
+    --ironsworn-color-bg-highlight: var(--ironsworn-color-fg);
+
+    transition: all 0.4s ease;
+    opacity: 0;
+    z-index: 0;
+    padding: var(--ironsworn-spacer-sm);
+  }
+
+  &:hover {
+    background-color: var(--ironsworn-color-clickable-block-bg-hover);
+
+    &::before {
+      background-color: ar(--ironsworn-color-fg);
+      opacity: 1;
+    }
+  }
+
+  .textStrokeMixin();
+}
+
 .statTitle {
   margin: var(--ironsworn-spacer-sm) 0;
   text-transform: uppercase;
@@ -24,7 +53,7 @@
 </style>
 
 <script setup lang="ts">
-import { computed, inject } from 'vue'
+import { capitalize, computed, inject } from 'vue'
 import { CharacterDataProperties } from '../../actor/actortypes'
 import { $ActorKey, ActorKey } from '../provisions'
 import IronBtn from './buttons/iron-btn.vue'
@@ -33,7 +62,14 @@ const { propKey } = defineProps<{
   propKey: string
 }>()
 
-const isMomentum = computed(() => propKey === 'momentum')
+const isMomentum = propKey === 'momentum'
+const i18nKey = `IRONSWORN.${capitalize(propKey)}`
+const i18nStat = game.i18n.localize(i18nKey)
+const tooltip = computed(() =>
+  isMomentum
+    ? undefined
+    : game.i18n.format('IRONSWORN.Roll +x', { stat: i18nStat })
+)
 
 const actor = inject(ActorKey)
 const actorSystem = computed(
