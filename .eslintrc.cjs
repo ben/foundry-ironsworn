23bc5f21ce86985fbd91c7fff133a7a9ee546f44
rsek <5354757+rsek@users.noreply.github.com>
2023-03-05 14:59:13 -0800

    js => cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  $schema: 'https://json.schemastore.org/eslintrc',
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+}
