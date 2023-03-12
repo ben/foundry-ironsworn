8175bc398fefec7fc289f465844ab2e49bed3d09
rsek <5354757+rsek@users.noreply.github.com>
2023-03-12 01:34:50 -0800

    dedicated list item component

---
+++ src/module/vue/components/list/sortable-list-item.vue
@@ -0,0 +1,40 @@
+<template>
+  <li :class="$style.wrapper">
+    <div class="flexcol nogrow order-buttons">
+      <!-- TODO: replace with better disabled attr? either aria-disabled or disabled attr -->
+      <IronBtn
+        icon="fa:caret-up"
+        block
+        nogrow
+        :class="{ disabled: i == 0 }"
+        @click="$emit('sortUp', i)"
+      ></IronBtn>
+      <IronBtn
+        icon="fa:caret-down"
+        block
+        nogrow
+        :class="{ disabled: i == length - 1 }"
+        @click="$emit('sortDown', i)"
+      ></IronBtn>
+    </div>
+    <slot name="default"></slot>
+  </li>
+</template>
+
+<style lang="scss" module>
+.wrapper {
+}
+.order-buttons {
+  padding-right: var(--ironsworn-spacer-sm);
+
+  .icon-button {
+    padding: var(--ironsworn-spacer-xs);
+  }
+}
+</style>
+
+<script lang="ts" setup>
+import IronBtn from '../buttons/iron-btn.vue'
+
+defineProps<{ i: number; length: number }>()
+</script>
