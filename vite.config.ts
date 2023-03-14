c4482df1c56b723c379c772b0077b56d3cb317c3
rsek <5354757+rsek@users.noreply.github.com>
2023-03-13 22:43:13 -0700

    begin migrating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,22 @@ import vue from '@vitejs/plugin-vue'
 import Inspector from 'vite-plugin-vue-inspector'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
+import sassChroma from './src/module/plugin/sass-chroma-js'
+import type Sass from 'sass'
+import presetEnv from 'postcss-preset-env'
 import { kebabCase } from 'lodash-es'
 import cssnano from 'cssnano'
-import presetEnv from 'postcss-preset-env'
+import sassIcons, { ICON_DIRS } from './src/module/plugin/custom-icons'
 
 const PORT = 30000
 
+const sassOptions: Sass.LegacyStringOptions<'sync'> = {
+	functions: { ...sassChroma, ...sassIcons },
+	additionalData: ''
+}
+
 const config: UserConfig = {
+	root: './',
 	plugins: [
 		vue(),
 		Inspector({ appendTo: 'src/index.ts', toggleComboKey: 'control-alt' }),
@@ -43,11 +52,10 @@ const config: UserConfig = {
 			}
 		}
 	},
+
 	css: {
 		preprocessorOptions: {
-			less: {
-				rewriteUrls: 'local'
-			}
+			scss: sassOptions
 		},
 		postcss: {
 			plugins: [presetEnv(), cssnano()]
