443a91adc4d5585b618813190e72eb3494287f5c
rsek <5354757+rsek@users.noreply.github.com>
2023-03-16 17:16:40 -0700

    Update src/module/vue/components/asset/asset-ability.vue
    Co-authored-by: Ben Straub <ben@straub.cc>

--- src/module/vue/components/asset/asset-ability.vue
+++ src/module/vue/components/asset/asset-ability.vue
@@ -56,20 +56,18 @@ const props = defineProps<{
 const canUpdate = computed(() => !!props.updateFn)
 
 const iconChecked = computed<IconSwitchState>(() => {
-	switch (IronswornSettings.starforgedToolsEnabled) {
-		case true:
-			return {
-				icon: 'fa:hexagon',
-				props: {
-					rotate: FontAwesome.Rotate['90deg'],
-					family: FontAwesome.Family.Solid
-				}
+	if (IronswornSettings.starforgedToolsEnabled) {
+		return {
+			icon: 'fa:hexagon',
+			props: {
+				rotate: FontAwesome.Rotate['90deg'],
+				family: FontAwesome.Family.Solid
 			}
-		default:
-			return {
-				icon: 'fa:circle',
-				props: { family: FontAwesome.Family.Solid }
-			}
+		}
+	}
+	return {
+		icon: 'fa:circle',
+		props: { family: FontAwesome.Family.Solid }
 	}
 })
 const iconUnchecked = computed<IconSwitchState>(() => {
