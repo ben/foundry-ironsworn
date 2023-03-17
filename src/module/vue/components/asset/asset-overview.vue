222fd6474c1be51d6bff364e3cf4790e096b347b
rsek <5354757+rsek@users.noreply.github.com>
2023-03-17 04:02:02 -0700

    plug in new components

--- src/module/vue/components/asset/asset-overview.vue
+++ src/module/vue/components/asset/asset-overview.vue
@@ -1,5 +1,5 @@
 <template>
-	<article class="flexcol ironsworn__asset" :class="articleClasses">
+	<AssetCard class="flexcol" :class="articleClasses">
 		<!--
         Semi-edit view:
         * Text entry for field VALUES (not names)
@@ -8,81 +8,65 @@
         * Track: name and value only
         * Conditions: checkboxes only
        -->
+		<template #header>
+			<AssetHeader class="nogrow flexrow">
+				<template #default="{ headerStyles }">
+					<span :class="headerStyles.type" aria-label="asset type">
+						{{ asset.system.category }}
+					</span>
+				</template>
+			</AssetHeader>
+		</template>
 
-		<header class="asset-header nogrow">
-			<span class="asset-type" aria-label="asset type">
-				{{ item.system.category }}
-			</span>
-		</header>
+		<template #default="{ styles }">
+			<section class="flexcol" :class="styles.body">
+				<!-- DESCRIPTION -->
+				<WithRollListeners
+					v-if="asset.system.description"
+					element="div"
+					class="nogrow"
+					@moveclick="moveClick"
+					v-html="$enrichHtml(asset.system.description)" />
 
-		<section class="asset-body flexcol">
-			<!-- DESCRIPTION -->
-			<WithRollListeners
-				v-if="item.system.description"
-				element="div"
-				class="nogrow"
-				@moveclick="moveClick"
-				v-html="$enrichHtml(item.system.description)" />
+				<!-- FIELDS -->
+				<div v-if="asset.system.fields?.length" :class="$styles.fields">
+					<AssetField
+						v-for="(field, i) in asset.system.fields"
+						:key="i"
+						:field="field"
+						:update-fn="(delta) => updateField(i, delta)"
+						class="nogrow" />
+				</div>
 
-			<!-- FIELDS -->
-			<div v-if="item.system.fields?.length" class="asset-fields">
-				<AssetField
-					v-for="(field, i) in item.system.fields"
-					:key="i"
-					:field="field"
-					:update-fn="(delta) => updateField(i, delta)"
-					class="nogrow" />
-			</div>
+				<!-- REQUIREMENT -->
+				<p
+					v-if="asset.system.requirement"
+					class="nogrow"
+					v-html="$enrichMarkdown(asset.system.requirement)"></p>
 
-			<!-- REQUIREMENT -->
-			<p
-				v-if="item.system.requirement"
-				class="nogrow"
-				v-html="$enrichMarkdown(item.system.requirement)"></p>
+				<!-- ABILITIES -->
+				<ul class="flexcol nogrow" :class="styles.abilities">
+					<li
+						v-for="(ability, i) in asset.system.abilities"
+						:key="`ability${i}`">
+						<AssetAbility
+							:ability="ability"
+							:update-fn="(delta) => updateAbility(i, delta)"
+							:toggle="true"
+							:readonly-clock="true"
+							class="flexrow" />
+					</li>
+				</ul>
 
-			<!-- ABILITIES -->
-			<ul class="asset-abilities flexcol nogrow">
-				<li v-for="(ability, i) in item.system.abilities" :key="`ability${i}`">
-					<AssetAbility
-						:ability="ability"
-						:update-fn="(delta) => updateAbility(i, delta)"
-						:toggle="true"
-						:readonly-clock="true"
-						class="flexrow" />
-				</li>
-			</ul>
+				<!-- OPTIONS -->
+				<AssetToggle
+					v-if="asset.system.exclusiveOptions.length > 0"
+					class="flexcol nogrow" />
 
-			<!-- OPTIONS -->
-			<section
-				v-if="item.system.exclusiveOptions.length > 0"
-				class="flexcol stack nogrow">
-				<AssetExclusiveoption
-					v-for="(opt, i) in item.system.exclusiveOptions"
-					:key="'option' + i"
-					:opt="opt"
-					@click="exclusiveOptionClick(i)" />
+				<AssetConditionMeter class="flexrow nogrow" />
 			</section>
-
-			<div class="flexrow nogrow">
-				<!-- TRACK -->
-				<ConditionMeterSlider
-					v-if="item.system.track.enabled"
-					slider-style="horizontal"
-					class="asset-condition-meter"
-					document-type="Item"
-					attr="track.current"
-					:current-value="item.system.track.current"
-					:max="item.system.track.max"
-					:min="0"
-					:stat-label="item.system.track.name"
-					label-position="left"
-					:read-only="false" />
-
-				<!-- CONDITIONS -->
-				<AssetConditions :asset="item" />
-			</div>
-		</section>
-	</article>
+		</template>
+	</AssetCard>
 </template>
 
 <script lang="ts" setup>
@@ -94,17 +78,18 @@ import type {
 	AssetField as AssetFieldType
 } from '../../../item/itemtypes'
 import WithRollListeners from '../with-rolllisteners.vue'
-import ConditionMeterSlider from '../resource-meter/condition-meter.vue'
-import AssetExclusiveoption from './asset-exclusiveoption.vue'
-import AssetConditions from './asset-conditions.vue'
 import AssetAbility from './asset-ability.vue'
 import AssetField from './asset-field.vue'
+import AssetHeader from './asset-header.vue'
+import AssetCard from './asset-card.vue'
+import AssetConditionMeter from './asset-condition-meter.vue'
+import AssetToggle from './asset-toggle.vue'
 
-const $item = inject($ItemKey)
-const item = inject(ItemKey) as ComputedRef
+const $asset = inject($ItemKey)
+const asset = inject(ItemKey) as ComputedRef
 
 const toolset = computed<'ironsworn' | 'starforged' | undefined>(
-	() => $item?.actor?.toolset ?? 'ironsworn'
+	() => $asset?.actor?.toolset ?? 'ironsworn'
 )
 
 const cssModule = useCssModule()
@@ -116,24 +101,24 @@ const articleClasses = computed(() => ({
 
 async function updateAbility(index: number, delta: Partial<AssetAbilityType>) {
 	const abilities = Object.values(
-		item.value.system.abilities
+		asset.value.system.abilities
 	) as AssetAbilityType[]
 	abilities[index] = mergeObject(abilities[index], delta) as AssetAbilityType
-	return $item?.update({ system: { abilities } })
+	return $asset?.update({ system: { abilities } })
 }
 
 async function updateField(index: number, delta: Partial<AssetFieldType>) {
-	const fields = Object.values(item.value.system.fields) as AssetFieldType[]
+	const fields = Object.values(asset.value.system.fields) as AssetFieldType[]
 	fields[index] = mergeObject(fields[index], delta) as AssetFieldType
-	return $item?.update({ system: { fields } })
+	return $asset?.update({ system: { fields } })
 }
 
 function exclusiveOptionClick(selectedIdx: number) {
-	const { exclusiveOptions } = item.value.system
+	const { exclusiveOptions } = asset.value.system
 	for (let i = 0; i < exclusiveOptions.length; i++) {
 		exclusiveOptions[i].selected = i === selectedIdx
 	}
-	$item?.update({ system: { exclusiveOptions } })
+	$asset?.update({ system: { exclusiveOptions } })
 }
 
 function moveClick(item) {
@@ -143,7 +128,7 @@ function moveClick(item) {
 
 <style lang="scss" module>
 .ironsworn__asset {
-	--ironsworn-color-thematic: v-bind('item.system.color');
+	--ironsworn-color-thematic: v-bind('asset.system.color');
 
 	margin: var(--ironsworn-spacer-xl) 0;
 	padding: var(--ironsworn-spacer-md);
