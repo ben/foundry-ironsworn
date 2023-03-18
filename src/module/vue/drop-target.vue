154c8da97750f7a7d24470584953d6cf3a464a45
rsek <5354757+rsek@users.noreply.github.com>
2023-03-17 21:14:42 -0700

    initial commit

--- src/module/vue/drop-target.vue
+++ src/module/vue/drop-target.vue
@@ -2,6 +2,7 @@
 	<component
 		v-bind="($attrs, $props)"
 		:is="is"
+		:class="$style.wrapper"
 		:data-ironsworn-drop-type="dropType"
 		:data-ironsworn-drop-active="state.active">
 		<slot />
@@ -35,3 +36,79 @@ onUnmounted(() => {
 	CONFIG.IRONSWORN.emitter.off('dragEnd', dragEnd)
 })
 </script>
+
+<style lang="scss" module>
+@use 'mixin:fx.scss';
+
+.wrapper {
+	--ironsworn-drop-outline-width: 3px;
+	--ironsworn-drop-outline-style: dotted;
+
+	// The duration of the transition that starts and ends the animation
+	--ironsworn-drop-transition-duration: 0.2s;
+
+	// The duration of the looped pulse animation
+	--ironsworn-drop-animation-duration: 0.8s;
+	--ironsworn-drop-animation-timing-function: ease-in-out;
+
+	position: relative;
+	transition: outline var(--ironsworn-drop-transition-duration) ease;
+	outline: 0 var(--ironsworn-drop-outline-style) transparent;
+
+	&::after {
+		@include fx.overlay;
+		@include fx.accentGradient(20);
+
+		transition: opacity var(--ironsworn-drop-transition-duration) ease;
+		visibility: hidden;
+		opacity: 0;
+		border-radius: inherit;
+	}
+
+	&[data-ironsworn-drop-active='true'] {
+		outline-offset: calc(-1 * var(--ironsworn-drop-outline-width));
+		outline-width: var(--ironsworn-drop-outline-width);
+		outline-color: var(--ironsworn-color-fg-warm-30);
+		animation: drop-zone-pulsate-outline
+			var(--ironsworn-drop-animation-duration)
+			var(--ironsworn-drop-animation-timing-function) infinite both;
+
+		&::after {
+			visibility: visible;
+			opacity: 1;
+			background-blend-mode: screen;
+			animation: drop-zone-pulsate-gradient
+				var(--ironsworn-drop-animation-duration)
+				var(--ironsworn-drop-animation-timing-function) infinite both;
+		}
+	}
+}
+
+@keyframes drop-zone-pulsate-outline {
+	0% {
+		outline-offset: calc(-1 * var(--ironsworn-drop-outline-width));
+	}
+
+	50% {
+		outline-offset: calc(-2 * var(--ironsworn-drop-outline-width));
+	}
+
+	100% {
+		outline-offset: calc(-1 * var(--ironsworn-drop-outline-width));
+	}
+}
+
+@keyframes drop-zone-pulsate-gradient {
+	0% {
+		opacity: 1;
+	}
+
+	50% {
+		opacity: 0.5;
+	}
+
+	100% {
+		opacity: 1;
+	}
+}
+</style>
