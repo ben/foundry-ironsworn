bbde65c967edb51c2538119decb1cedaf7d86e85
rsek <5354757+rsek@users.noreply.github.com>
2023-03-19 04:06:36 -0700

    keybindings

--- src/module/vue/components/rank-pips/rank-pips.vue
+++ src/module/vue/components/rank-pips/rank-pips.vue
@@ -1,44 +1,159 @@
 <template>
 	<article
-		class="rank-pips"
+		:class="$style.wrapper"
 		:aria-label="$t('IRONSWORN.ChallengeRank')"
-		:aria-valuetext="localizeRank(current)">
+		:aria-valuetext="localizeRank(current)"
+		:aria-valuemin="RANK_MIN"
+		:aria-valuemax="RANK_MAX"
+		:aria-valuenow="current"
+		aria-orientation="horizontal"
+		tabindex="0"
+		:aria-readonly="readonly"
+		role="spinner"
+		@keydown.arrow-up="setRank(current + 1)"
+		@keydown.+="setRank(current + 1)"
+		@keydown.arrow-left="setRank(current + 1)"
+		@keydown.page-up="setRank(current + 2)"
+		@keydown.-="setRank(current - 1)"
+		@keydown.arrow-down="setRank(current - 1)"
+		@keydown.arrow-right="setRank(current - 1)"
+		@keydown.page-down="setRank(current - 2)"
+		@keydown.home="setRank(RANK_MIN)"
+		@keydown.end="setRank(RANK_MAX)"
+		@keydown.1="setRank(1)"
+		@keydown.2="setRank(2)"
+		@keydown.3="setRank(3)"
+		@keydown.4="setRank(4)"
+		@keydown.5="setRank(5)">
 		<button
 			v-for="rank in 5"
 			:key="rank"
 			:data-tooltip="localizeRank(rank)"
 			data-tooltip-direction="UP"
 			type="button"
-			class="rank-pip nogrow theme-pip"
+			class="nogrow"
+			tabindex="-1"
+			@focus.prevent
 			:aria-selected="rank === current"
-			@click="$emit('click', rank)">
-			<PipSvgCircle
-				v-if="pipStyle === 'circle'"
+			:data-pip-state="getState(rank)"
+			@mouseover="hovered = rank"
+			@mouseleave="hovered = 0"
+			@click="setRank(rank)"
+			:class="$style.pip">
+			<FontIcon
+				v-bind="deco.challengeRank.checked"
 				role="presentational"
-				class="svg pip-shape" />
-			<PipSvgHex
-				v-if="pipStyle === 'hex'"
+				:fw="false"
+				:class="[$style.checked, $style.icon]" />
+			<FontIcon
+				v-bind="deco.challengeRank.unchecked"
 				role="presentational"
-				class="svg pip-shape" />
+				:fw="false"
+				:class="[$style.unchecked, $style.icon]" />
 		</button>
 	</article>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import PipSvgHex from './pip-svg-hex.vue'
-import PipSvgCircle from './pip-svg-circle.vue'
+import { computed, ref } from 'vue'
 import { IronswornSettings } from '../../../helpers/settings.js'
-import type { ChallengeRank } from '../../../constants'
+import { ChallengeRank } from '../../../constants'
 import { localizeRank } from '../../../helpers/util'
+import FontIcon from '../icon/font-icon.vue'
+import { DECORATION } from '../../../../decoration'
+import { clamp } from 'lodash'
 
-defineProps<{
-	current: ChallengeRank
-}>()
-
-const pipStyle = computed(() =>
-	IronswornSettings.get('theme') === 'starforged' ? 'hex' : 'circle'
+const props = withDefaults(
+	defineProps<{
+		current: ChallengeRank
+		readonly?: boolean
+	}>(),
+	{ readonly: false }
 )
 
-defineEmits(['click'])
+const hovered = ref(0)
+
+const deco = computed(() =>
+	IronswornSettings.starforgedToolsEnabled
+		? DECORATION.Starforged
+		: DECORATION.Ironsworn
+)
+
+type PipState = 'filled' | 'empty' | 'preview'
+
+function getState(rank: ChallengeRank): PipState {
+	switch (true) {
+		case hovered.value === 0 && rank <= props.current:
+			return 'filled'
+		case rank <= hovered.value:
+			return 'preview'
+		default:
+			return 'empty'
+	}
+}
+
+const RANK_MIN = ChallengeRank.Troublesome
+const RANK_MAX = ChallengeRank.Epic
+
+const $emit = defineEmits<{ (event: 'change', newRank: ChallengeRank): void }>()
+
+function setRank(rank: ChallengeRank) {
+	if (props.readonly) return
+	$emit('change', clamp(rank, RANK_MIN, RANK_MAX))
+}
 </script>
+<style lang="scss" module>
+.wrapper {
+	--ironsworn-pip-spacing: 1px;
+	--fa-display: block;
+	display: flex;
+	flex-grow: 0;
+	align-items: center;
+	width: max-content;
+	height: max-content;
+	&[aria-readonly='true'] {
+		pointer-events: none !important;
+	}
+}
+
+.pip {
+	display: grid;
+	box-shadow: none !important;
+	background: none;
+	// use padding isntead of margins for one continuous surface -- avoids hover flicker
+	padding: calc(var(--ironsworn-pip-spacing) / 2);
+	height: max-content;
+	width: max-content;
+}
+
+.icon {
+	display: block !important;
+	// stacks icons on top of each other by assigning them to the same grid cell
+	grid-row: 1;
+	grid-column: 1;
+	transition: opacity var(--ironsworn-transition-timing);
+}
+
+.checked {
+	opacity: 0;
+}
+.unchecked {
+}
+
+[data-pip-state='filled'] {
+	:local(.checked) {
+		opacity: 1;
+	}
+}
+
+[data-pip-state='preview'] {
+	:local(.checked) {
+		opacity: 0.5;
+	}
+	&:active {
+		:local(.checked) {
+			opacity: 1;
+		}
+	}
+}
+</style>
