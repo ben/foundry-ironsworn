aaf1da13cc89c9848535f426403e9e999113bb2d
rsek <5354757+rsek@users.noreply.github.com>
2023-03-20 01:29:11 -0700

    skip setting a variable since its only used once

--- src/module/features/ironcolor.ts
+++ src/module/features/ironcolor.ts
@@ -6,11 +6,9 @@ import '../../styles/_ironcolor/classic.scss'
 import '../../styles/_ironcolor/phosphor.scss'
 
 export function colorSchemeSetup() {
-	const colorScheme = IronswornSettings.get('color-scheme')
-
 	$(document.body).addClass(IronswornSettings.classes.join(' '))
 
-	if (colorScheme === 'phosphor') {
+	if (IronswornSettings.get('color-scheme') === 'phosphor') {
 		// TODO: figure out a way to automate this without calling it out for every single theme.
 		;(<string[]>CONFIG.TinyMCE.content_css)?.push(
 			'/systems/foundry-ironsworn/styles/starforged-tinymce.css'
