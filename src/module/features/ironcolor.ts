bf3f4d30112d987f0bb6a6fa9bb4ee0f69511e6d
rsek <5354757+rsek@users.noreply.github.com>
2023-03-20 02:30:46 -0700

    adjust annotation

--- src/module/features/ironcolor.ts
+++ src/module/features/ironcolor.ts
@@ -9,7 +9,7 @@ export function colorSchemeSetup() {
 	$(document.body).addClass(IronswornSettings.classes.join(' '))
 
 	if (IronswornSettings.get('color-scheme') === 'phosphor') {
-		// TODO: figure out a way to automate this without calling it out for every single theme.
+		// TODO: figure out a way to automate this without calling it out for every single color scheme.
 		;(<string[]>CONFIG.TinyMCE.content_css)?.push(
 			'/systems/foundry-ironsworn/styles/starforged-tinymce.css'
 		)
