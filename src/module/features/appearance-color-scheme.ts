7ca783a014cd43949a74109f886b086f0906a702
rsek <5354757+rsek@users.noreply.github.com>
2023-03-19 22:35:47 -0700

    rm redundant classes, they're applied at top level

--- src/module/features/appearance-color-scheme.ts
+++ src/module/features/appearance-color-scheme.ts
@@ -1,13 +1,14 @@
 import { IronswornSettings } from '../helpers/settings'
 
 import '../../styles/styles.less'
-import '../../styles/themes/classic.less'
-import '../../styles/themes/phosphor.less'
+import '../../styles/themes/themes.scss'
+import '../../styles/color-schemes/classic.scss'
+import '../../styles/color-schemes/phosphor.scss'
 
 export function colorSchemeSetup() {
 	const colorScheme = IronswornSettings.get('color-scheme')
 
-	$(document.body).addClass(IronswornSettings.colors)
+	$(document.body).addClass(IronswornSettings.classes.join(' '))
 
 	if (colorScheme === 'phosphor') {
 		// TODO: figure out a way to automate this without calling it out for every single theme.
