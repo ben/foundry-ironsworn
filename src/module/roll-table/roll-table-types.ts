7944cc4d1e9c5eebd7eab0dbc80119ff498c2cc9
rsek <5354757+rsek@users.noreply.github.com>
2023-04-10 02:05:53 -0700

    roll table types

---
+++ src/module/roll-table/roll-table-types.ts
@@ -0,0 +1,87 @@
+import type EmbeddedCollection from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/embedded-collection.mjs'
+import type { OracleTableResult } from './table-result'
+import type { OracleTable } from './roll-table'
+import type {
+	RollTableData,
+	TableResultData
+} from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/module.mjs'
+
+// Ironsworn-specific augmentations
+
+/** "Fake" types used to distinguish certain kinds of computed OracleTableResults. They're set in OracleTableResult#flags['foundry-ironsworn'].type */
+export type ComputedTableResultType =
+	| 'delve-site-denizen'
+	| 'delve-site-feature'
+	| 'delve-site-danger'
+	| 'truth-option'
+	| 'truth-option-subtable-result'
+/** "Fake" types used to distinguish certain kinds of computed OracleTables. They're set in OracleTable#flags['foundry-ironsworn'].type */
+export type ComputedTableType =
+	| 'delve-site-denizens'
+	| 'delve-site-features'
+	| 'delve-site-dangers'
+	| 'truth-options'
+	| 'truth-option-subtable'
+
+declare global {
+	interface FlagConfig {
+		RollTable: {
+			'foundry-ironsworn'?: {
+				/** The UUID of the originating document, for computed RollTables */
+				sourceId?: Actor['uuid'] | Item['uuid'] | null | undefined
+				type?: ComputedTableType
+				/** A subtitle to be included in smaller text above a chat message's header. Standard oracles default to displaying their category flag, so they don't need to set this unless an override is needed.
+				 */
+				subtitle?: string | null | undefined
+				/** The Dataforged ID associated with this oracle. */
+				dfid?: string
+				/** The category associated with the Dataforged oracle. */
+				category?: string
+			}
+		}
+		TableResult: {
+			'foundry-ironsworn'?: {
+				/** The UUID of the originating document, for computed TableResults */
+				sourceId?: Actor['uuid'] | Item['uuid'] | null | undefined
+				type?: ComputedTableResultType
+			}
+		}
+	}
+
+	interface BaseRollTable {
+		get results(): EmbeddedCollection<
+			CONFIG['TableResult']['documentClass'],
+			foundry.data.RollTableData
+		>
+	}
+	interface DocumentClassConfig {
+		TableResult: typeof OracleTableResult
+		RollTable: typeof OracleTable
+	}
+
+	interface RollTableDraw {
+		roll: Roll
+		results: Array<InstanceType<CONFIG['TableResult']['documentClass']>>
+	}
+}
+
+// Augmentations to fix stuff missing from LoFD types
+declare global {
+	// fixes numerous missing properties caused by LoFD types having inheritance chains inconsistent with FVTT's source code
+	interface TableResult
+		extends Omit<
+			TableResultData,
+			'_initialize' | 'toJSON' | 'update' | 'toObject' | 'collection'
+		> {}
+	interface RollTable
+		extends Omit<
+			RollTableData,
+			| 'update'
+			| 'toObject'
+			| 'toJSON'
+			| 'results'
+			| 'permission'
+			| 'name'
+			| 'folder'
+		> {}
+}
