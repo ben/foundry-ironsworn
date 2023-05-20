739e24817e120b77a355bf2d2385660e0f55dc93
rsek <5354757+rsek@users.noreply.github.com>
2023-05-19 17:06:41 -0700

    rank-specific _cast

--- src/module/fields/ChallengeRankField.ts
+++ src/module/fields/ChallengeRankField.ts
@@ -1,12 +1,9 @@
 import { ChallengeRank } from '../constants'
-import { localizeRank } from '../helpers/util'
+import { capitalize } from '../helpers/util'
 import { enumEntries } from '../fields/utils'
 
 export class ChallengeRankField extends foundry.data.fields
 	.NumberField<ChallengeRank> {
-	/**
-	 *
-	 */
 	constructor(
 		options?: Partial<
 			Omit<
@@ -31,5 +28,21 @@ export class ChallengeRankField extends foundry.data.fields
 			min: ChallengeRank.Troublesome,
 			...options
 		})
+	}
+
+	override _cast(value: unknown) {
+		switch (true) {
+			// migration: "formidible" -> "formidable"
+			// TODO: use this instead of migration #1
+			case value === 'formidible':
+				return ChallengeRank.Formidable
+			// migration: string-based challenge ranks to numeric ones
+			// TODO: use this instead of migration #5
+			case typeof value === 'string':
+				return ChallengeRank[capitalize(value as string) as keyof ChallengeRank]
+			default: {
+				return super._cast(value)
+			}
+		}
 	}
 }
