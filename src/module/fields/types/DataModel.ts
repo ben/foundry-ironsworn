6f1fe3e96297d2dd60d4d81129a6c759871a357a
rsek <5354757+rsek@users.noreply.github.com>
2023-06-06 20:53:01 -0700

    add annotation, a couple types

--- src/module/fields/types/DataModel.ts
+++ src/module/fields/types/DataModel.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 
 import type { Document } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/module.mjs'
-import {
+import type {
 	ModuleData,
 	SystemData
 } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/packages.mjs'
@@ -601,6 +601,63 @@ declare global {
 					| Record<string, DataModelValidationFailure>
 			}
 
+			/**
+			 * A specialized subclass of DataModel, intended to represent a Document's type-specific data.
+			 * Systems or Modules that provide DataModel implementations for sub-types of Documents (such as Actors or Items)
+			 * should subclass this class instead of the base DataModel class.
+			 *
+			 * @see {@link Document}
+			 *
+			 * @example Registering a custom sub-type for a Module.
+			 *
+			 * **module.json**
+			 * ```json
+			 * {
+			 *   "id": "my-module",
+			 *   "esmodules": ["main.mjs"],
+			 *   "documentTypes": {
+			 *     "Actor": {
+			 *       "sidekick": {},
+			 *       "villain": {}
+			 *     },
+			 *     "JournalEntryPage": {
+			 *       "dossier": {},
+			 *       "quest": {
+			 *         "htmlFields": ["description"]
+			 *       }
+			 *     }
+			 *   }
+			 * }
+			 * ```
+			 *
+			 * **main.mjs**
+			 * ```js
+			 * Hooks.on("init", () => {
+			 *   Object.assign(CONFIG.Actor.dataModels, {
+			 *     "my-module.sidekick": SidekickModel,
+			 *     "my-module.villain": VillainModel
+			 *   });
+			 *   Object.assign(CONFIG.JournalEntryPage.dataModels, {
+			 *     "my-module.dossier": DossierModel,
+			 *     "my-module.quest": QuestModel
+			 *   });
+			 * });
+			 *
+			 * class QuestModel extends foundry.abstract.TypeDataModel {
+			 *   static defineSchema() {
+			 *     const fields = foundry.data.fields;
+			 *     return {
+			 *       description: new fields.HTMLField({required: false, blank: true, initial: ""}),
+			 *       steps: new fields.ArrayField(new fields.StringField())
+			 *     };
+			 *   }
+			 *
+			 *   prepareDerivedData() {
+			 *     this.totalSteps = this.steps.length;
+			 *   }
+			 * }
+			 * ```
+			 */
 			export abstract class TypeDataModel<
 				ConcreteData extends object,
 				Parent extends AnyDocument
