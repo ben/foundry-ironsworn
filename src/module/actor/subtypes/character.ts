8393d017d7b5326de036bd98563cfe103bb355d8
rsek <5354757+rsek@users.noreply.github.com>
2023-06-09 01:03:54 -0700

    replace canBurnMomentum; adjust methods

--- src/module/actor/subtypes/character.ts
+++ src/module/actor/subtypes/character.ts
@@ -2,7 +2,6 @@ import { StatField } from '../../fields/StatField'
 import type { IronswornActor } from '../actor'
 import { ProgressTicksField } from '../../fields/ProgressTicksField'
 import type { DataSchema } from '../../fields/utils'
-import type { ActorDataSource } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/actorData'
 import type { ActiveEffectDataConstructorData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/activeEffectData'
 import { IronActiveEffect } from '../../active-effect/active-effect'
 import { ConditionMeterField, MomentumField } from '../../fields/MeterField'
@@ -26,28 +25,36 @@ export class CharacterData extends foundry.abstract.TypeDataModel<
 		>
 	) {
 		super(...args)
+		this.resetMomentum = this.resetMomentum.bind(this)
 		this.burnMomentum = this.burnMomentum.bind(this)
 	}
 
-	async burnMomentum(this: CharacterData) {
-		if (this.canBurnMomentum) {
-			await this.parent.update({
-				system: { 'momentum.value': this.parent.system.momentum.resetValue }
-			})
-		}
+	get canBurnMomentum() {
+		return this.momentum.value >= MomentumField.BURN_MIN
 	}
 
+	async resetMomentum() {
+		return await this.parent.update({
+			'system.momentum.value': this.parent.system.momentum.resetValue
+		})
+	}
+
+	async burnMomentum(this: CharacterData) {
+		if (this.canBurnMomentum) await this.resetMomentum()
+	}
+
+	/** A convenience getter that returns all custom impactsts on the PC. */
 	get customImpacts() {
-		const customImpactIDs = ['custom1', 'custom2']
+		const customImpactIDs = ['custom1', 'custom2'] as const
 		return this.parent.effects.filter((impact) =>
 			customImpactIDs.some((id) => (impact as any).statuses.has(id))
 		)
 	}
 
 	static override migrateData(source: Record<string, unknown>) {
-		source = super.migrateData(source) as Record<string, unknown> &
-			ActorDataSource
+		source = super.migrateData(source)
 
+		// Migrate boolean debility record object to ActiveEffect-based impacts
 		if ('debility' in source) {
 			const debilities = source.debility as Record<string, boolean | string>
 			if (source.effects == null)
@@ -69,11 +76,14 @@ export class CharacterData extends foundry.abstract.TypeDataModel<
 
 			for (const [key, value] of Object.entries(debilities)) {
 				if (key.startsWith('custom') || value !== true) continue
-				;(source.effects as any[]).push(
-					foundry.utils.deepClone(
-						CONFIG.statusEffects.find((fx) => fx.id === key)
-					)
-				)
+				const foundEffect =
+					CONFIG.statusEffects.find((fx) => fx.id === key) ??
+					// not found -- try broadening the search
+					Object.values(IronActiveEffect.statusEffects)
+						.flat()
+						.find((fx) => fx.id === key)
+				if (foundEffect == null) continue
+				;(source.effects as any[]).push(foundry.utils.deepClone(foundEffect))
 			}
 
 			delete source.debility
@@ -95,6 +105,7 @@ export class CharacterData extends foundry.abstract.TypeDataModel<
 			wits: new StatField({ label: 'IRONSWORN.Wits' }),
 
 			// TODO: add a localized `hint` property, and have the vue sheet automatically pull these in as tooltips
+			// TODO: pass the labels to $capitalize ahead of time
 			health: new ConditionMeterField({ label: 'IRONSWORN.Health' }),
 			spirit: new ConditionMeterField({ label: 'IRONSWORN.Spirit' }),
 			supply: new ConditionMeterField({ label: 'IRONSWORN.Supply' }),
