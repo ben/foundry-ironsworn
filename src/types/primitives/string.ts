7dca906bd3f6e166b7192565890f3461540ac4ea
rsek <5354757+rsek@users.noreply.github.com>
2023-06-16 19:56:48 -0700

    add string primitives

---
+++ src/types/primitives/string.ts
@@ -0,0 +1,27 @@
+// from common/primitives/string.mjs
+declare global {
+	interface String {
+		/**
+		 * Capitalize a string, transforming it's first character to a capital letter.
+		 */
+		capitalize(): string
+		/**
+		 * Convert a string to Title Case where the first letter of each word is capitalized.
+		 */
+		titleCase(): string
+		/**
+		 * Strip any script tags which were included within a provided string.
+		 */
+		stripScripts(): string
+		/**
+		 * Transform any string into an url-viable slug string
+		 * @param options      Optional arguments which customize how the slugify operation is performed
+		 * @param replacement  The replacement character to separate terms, default is '-'
+		 * @param strict    Replace all non-alphanumeric characters, or allow them? Default false
+		 * @returns               The slugified input string
+		 */
+		slugify(options?: { replacement?: string; strict?: boolean }): string
+	}
+}
+
+export {}
