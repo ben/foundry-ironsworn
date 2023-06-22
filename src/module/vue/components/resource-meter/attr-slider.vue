ef868e46117273fdbb46cc5596f73964fb71b177
rsek <5354757+rsek@users.noreply.github.com>
2023-06-21 23:43:22 -0700

    typecheck appeasement

--- src/module/vue/components/resource-meter/attr-slider.vue
+++ src/module/vue/components/resource-meter/attr-slider.vue
@@ -86,14 +86,15 @@ const field = computed(
 		$document?.system.schema.getField(props.attr) as
 			| AssetConditionMeterField
 			| MeterField
-			| foundry.data.fields.NumberField
 )
 
 const min = computed(
+	// @ts-expect-error
 	() => document?.value.system[props.attr].min ?? field.value.min ?? 0
 )
 
 const max = computed(() => {
+	// @ts-expect-error
 	const fieldMax = field.value.max
 	const currentMax = document?.value?.system[props.attr].max as
 		| number
