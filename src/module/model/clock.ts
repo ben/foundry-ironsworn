769448b87db2825660e77b9d5c33d1c512ce7261
rsek <5354757+rsek@users.noreply.github.com>
2023-06-23 16:28:19 -0700

    provide statics on class as constants

--- src/module/model/clock.ts
+++ src/module/model/clock.ts
@@ -4,22 +4,25 @@ export class Clock<
 	Parent extends foundry.abstract.DataModel.AnyOrDoc = foundry.abstract.DataModel.AnyOrDoc
 > extends foundry.abstract.DataModel<ClockSource, ClockSource, Parent> {
 	static readonly MIN = 0
+	static readonly MAX = 12
+	static readonly SIZES = [4, 6, 8, 10, 12]
+	static readonly SIZE_MIN = 4
 
 	static override defineSchema(): DataSchema<ClockSource> {
 		return {
 			value: new foundry.data.fields.NumberField({
-				initial: 0,
+				initial: Clock.MIN,
 				integer: true,
-				min: 0,
-				max: 12,
+				min: Clock.MIN,
+				max: Clock.MAX,
 				label: 'IRONSWORN.SegmentsFilled'
 			}),
 			max: new foundry.data.fields.NumberField({
-				initial: 4,
+				initial: Clock.SIZE_MIN,
 				integer: true,
-				choices: [4, 6, 8, 10, 12],
-				min: 4,
-				max: 12,
+				choices: Clock.SIZES as any[],
+				min: Clock.SIZE_MIN,
+				max: Clock.MAX,
 				label: 'IRONSWORN.SegmentsMax'
 			}),
 			enabled: new foundry.data.fields.BooleanField({
