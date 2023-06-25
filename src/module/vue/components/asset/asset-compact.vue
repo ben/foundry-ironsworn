9c69a66879a16ead5b9dcc9f41df236a8b53f6bb
rsek <5354757+rsek@users.noreply.github.com>
2023-06-25 11:41:14 -0700

    restore asset delete btn

--- src/module/vue/components/asset/asset-compact.vue
+++ src/module/vue/components/asset/asset-compact.vue
@@ -6,7 +6,7 @@
 		@toggle-expand="toggle">
 		<template #headerEnd>
 			<div class="flexrow nogrow" :class="$style.controls">
-				<IronBtn v-if="editMode" block nogrow :document="$asset" />
+				<BtnDocDelete v-if="editMode" block nogrow :document="$asset" />
 				<IronBtn block nogrow icon="fa:pen-to-square" @click="edit" />
 			</div>
 		</template>
@@ -19,14 +19,18 @@ import { computed, inject, provide } from 'vue'
 import { $ActorKey, $ItemKey, ActorKey, ItemKey } from '../../provisions'
 import IronBtn from 'component:buttons/iron-btn.vue'
 import AssetCard from 'component:asset/asset-card.vue'
+import BtnDocDelete from '../buttons/btn-doc-delete.vue'
+import type { IronswornItem } from '../../../item/item'
 
-const props = defineProps<{ asset: any }>()
+const props = defineProps<{ asset: ItemSource<'asset'> }>()
 const actor = inject(ActorKey) as Ref
 
 const $actor = inject($ActorKey)
-const $asset = $actor
-	? $actor?.items.find((x) => x.id === props.asset._id)
-	: game.items?.get(props.asset._id)
+const $asset = (
+	$actor
+		? $actor?.items.find((x) => x.id === props.asset._id)
+		: game.items?.get(props.asset._id as string)
+) as IronswornItem<'asset'>
 
 provide($ItemKey, $asset)
 provide(
