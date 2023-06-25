796aafb1b11c359757bc8ed32e33848aa54c555d
rsek <5354757+rsek@users.noreply.github.com>
2023-06-25 12:48:17 -0700

    fwd changes from progress model branch

--- src/module/fields/utils.ts
+++ src/module/fields/utils.ts
@@ -61,8 +61,30 @@ export type SourceData<
 > = ReturnType<DocumentInstance['toObject']> &
 	Extract<SourceConfig[DocumentInstance['documentName']], { type: Subtype }>
 
-export type DataSchema<
-	T extends Record<string, any> = Record<string, unknown>
-> = {
-	[K in keyof T]-?: foundry.data.fields.DataField<any, T[K], any>
+export type DataSchema<SourceData, ConcreteData = SourceData> = {
+	[K in keyof ConcreteData]-?: SelectFieldTypeFor<
+		K extends keyof SourceData ? NonNullish<SourceData[K]> : any,
+		NonNullish<ConcreteData[K]>
+	>
 }
+
+type NonNullish<T> = Exclude<T, undefined | null>
+
+type SelectFieldTypeFor<SourceData, ConcreteData> =
+	ConcreteData extends foundry.abstract.DataModel.Any
+		? foundry.data.fields.EmbeddedDataField<any, ConcreteData, any, any>
+		: SourceData extends number
+		? foundry.data.fields.NumberField<number, number, any>
+		: SourceData extends boolean
+		? foundry.data.fields.BooleanField
+		: SourceData extends string
+		? ConcreteData extends string
+			? foundry.data.fields.StringField<string, string, any>
+			: ConcreteData extends foundry.abstract.Document<any, any, any>
+			? foundry.data.fields.StringField<string, ConcreteData, any>
+			: foundry.data.fields.StringField<string, any, any>
+		: SourceData extends any[]
+		? foundry.data.fields.ArrayField<any, SourceData, any>
+		: SourceData extends object
+		? foundry.data.fields.SchemaField<SourceData, ConcreteData, any>
+		: foundry.data.fields.DataField<SourceData, ConcreteData, any>
