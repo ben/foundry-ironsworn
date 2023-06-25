1763b3c51e7aca9b45760fb94062868d86201575
rsek <5354757+rsek@users.noreply.github.com>
2023-06-24 22:44:29 -0700

    cleanup some unused declarations

--- src/module/vue/components/buttons/btn-oracle.vue
+++ src/module/vue/components/buttons/btn-oracle.vue
@@ -15,24 +15,21 @@
 
 <script setup lang="ts">
 import { sample } from 'lodash-es'
-
-import { inject } from 'vue'
 import type { IOracleTreeNode } from '../../../features/customoracles.js'
 import { OracleTable } from '../../../roll-table/oracle-table'
 import IronBtn from './iron-btn.vue'
 
-interface Props extends Omit<PropsOf<typeof IronBtn>, 'tooltip'> {}
-
-const props = defineProps<{
+interface Props extends Omit<PropsOf<typeof IronBtn>, 'tooltip'> {
 	node: IOracleTreeNode
 	overrideClick?: boolean
 	// Hack: if we declare `click` in the emits, there's no $attrs['onClick']
 	// This allows us to check for presence and still use $emit('click')
 	// https://github.com/vuejs/core/issues/4736#issuecomment-934156497
 	onClick?: Function
-}>()
+}
 
-const toolset = inject<'ironsworn' | 'starforged'>('toolset')
+const props = defineProps<Props>()
+
 const $emit = defineEmits(['click'])
 
 async function rollOracle() {
