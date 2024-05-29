460bcb65c52d033689d3f082daab0c858668ab8b
Ben Straub <ben@straub.cc>
2024-05-29 07:11:43 -0700

    Fix oracle rolls and 3d dice

--- src/module/roll-table/oracle-table.ts
+++ src/module/roll-table/oracle-table.ts
@@ -329,6 +329,7 @@ export class OracleTable extends RollTable {
 						? CONST.CHAT_MESSAGE_TYPES.ROLL
 						: CONST.CHAT_MESSAGE_TYPES.OTHER,
 				roll,
+				rolls: [roll],
 				sound: roll != null ? CONFIG.sounds.dice : null,
 				flags
 			},
