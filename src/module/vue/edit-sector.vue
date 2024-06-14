f86e4267571279c0b44ea9c70657c735ae1347e5
Ben Straub <ben@straub.cc>
2024-06-14 10:35:27 -0700

    Edit-scene-dialog updates for SI

--- src/module/vue/edit-sector.vue
+++ src/module/vue/edit-sector.vue
@@ -4,25 +4,26 @@
 			{{ $t('IRONSWORN.Region') }}
 			<i class="fa fa-circle-question" data-tooltip="IRONSWORN.RegionTip"></i>
 		</h4>
-		<label class="nogrow">
-			<input v-model="region" type="radio" value="terminus" />
-			{{ $t('IRONSWORN.REGION.Terminus') }}
-		</label>
-		<label class="nogrow">
-			<input v-model="region" type="radio" value="outlands" />
-			{{ $t('IRONSWORN.REGION.Outlands') }}
-		</label>
-		<label class="nogrow">
-			<input v-model="region" type="radio" value="expanse" />
-			{{ $t('IRONSWORN.REGION.Expanse') }}
+		<label class="nogrow" v-for="option in options">
+			<input v-model="region" type="radio" :value="option.toLowerCase()" />
+			{{ $t(`IRONSWORN.REGION.${option}`) }}
 		</label>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { watch, ref } from 'vue'
+import { IronswornSettings } from '../helpers/settings'
 
 const props = defineProps<{ data: { sceneId: string } }>()
+
+const defaultToolbox = IronswornSettings.defaultToolbox
+const options: string[] =
+	defaultToolbox === 'starforged'
+		? ['Terminus', 'Outlands', 'Expanse']
+		: defaultToolbox === 'sunderedisles'
+		? ['Myriads', 'Margins', 'Reaches']
+		: []
 
 const scene = game.scenes?.get(props.data.sceneId)
 const region = ref<string>(scene?.flags['foundry-ironsworn']?.['region'])
