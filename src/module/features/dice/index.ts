8344886e97188d30be5309392746735833b16d78
Ben Straub <ben@straub.cc>
2024-06-16 07:51:23 -0800

    Add extra die to DSN

--- src/module/features/dice/index.ts
+++ src/module/features/dice/index.ts
@@ -3,3 +3,26 @@ import { DieCursed } from './cursed-die'
 Hooks.once('init', function () {
 	CONFIG.Dice.terms['s'] = DieCursed
 })
+
+Hooks.once('diceSoNiceReady', (dice3d) => {
+	dice3d.addSystem({ id: 'ironsworn', name: 'Ironsworn' }, 'preferred')
+	dice3d.addDicePreset(
+		{
+			system: 'ironsworn',
+			type: 'ds',
+			labels: [
+				'systems/foundry-ironsworn/assets/dice/dcursed-cursed.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp',
+				'systems/foundry-ironsworn/assets/dice/dcursed-blank.webp'
+			]
+		},
+		'd10'
+	)
+})
