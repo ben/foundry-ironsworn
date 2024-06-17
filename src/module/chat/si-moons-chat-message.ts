968431eca7b32b3094e2d3204743188edccc02cf
Ben Straub <ben@straub.cc>
2024-06-17 07:52:36 -0800

    Better chat-message format

--- src/module/chat/si-moons-chat-message.ts
+++ src/module/chat/si-moons-chat-message.ts
@@ -1,6 +1,25 @@
+const MOON_EMOJI = [
+	'',
+	'🌑',
+	'🌒',
+	'🌒',
+	'🌒',
+	'🌓',
+	'🌓',
+	'🌔',
+	'🌔',
+	'🌔',
+	'🌕'
+]
+
 export async function createSiMoonsChatMessage(cinder: Roll, wraith: Roll) {
 	// TODO: resolve which one is higher, matches, etc.
-	const params = { cinder, wraith }
+	const params = {
+		cinder,
+		cinderEmoji: MOON_EMOJI[cinder.total ?? 1],
+		wraith,
+		wraithEmoji: MOON_EMOJI[wraith.total ?? 1]
+	}
 	const content = await renderTemplate(
 		'systems/foundry-ironsworn/templates/rolls/si-moons-roll-message.hbs',
 		params
