503f2603caca24f53330e73cc5759468d6f472e4
Ben Straub <ben@straub.cc>
2024-08-25 17:05:11 -0700

    Fix import paths

--- src/module/datasworn2/import/ids.ts
+++ src/module/datasworn2/import/ids.ts
@@ -2,15 +2,15 @@ import shajs from 'sha.js'
 
 // Import a local copy of the legacy ID maps
 export const LegacyToDataswornIds: Record<string, string> = {
-	...require('../../../../datasworn/src/legacy_ids/dataforged/classic/assets.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/classic/moves.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/classic/npcs.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/classic/oracles.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/starforged/assets.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/starforged/moves.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/starforged/oracles.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/starforged/npcs.json'),
-	...require('../../../../datasworn/src/legacy_ids/dataforged/starforged/truths.json')
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/classic/assets.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/classic/moves.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/classic/npcs.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/classic/oracles.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/starforged/assets.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/starforged/moves.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/starforged/oracles.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/starforged/npcs.json'),
+	...require('../../../../../datasworn/src/legacy_ids/dataforged/starforged/truths.json')
 }
 export const DataswornToLegacyIds: Record<string, string> = Object.fromEntries(
 	Object.entries(LegacyToDataswornIds).map(([k, v]) => [v, k])
