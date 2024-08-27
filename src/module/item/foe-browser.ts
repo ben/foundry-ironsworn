7abe5b63969370d433f0a2edceee6c891ca438e6
Ben Straub <ben@straub.cc>
2024-08-26 20:06:16 -0700

    A quick start

---
+++ src/module/item/foe-browser.ts
@@ -0,0 +1,18 @@
+import FoeBrowserVue from '../vue/foe-browser.vue'
+import { VueAppMixin } from '../vue/vueapp.js'
+
+export class FoeBrowser extends VueAppMixin(Application) {
+	constructor() {
+		super({})
+	}
+
+	static get defaultOptions() {
+		return foundry.utils.mergeObject(super.defaultOptions, {
+			title: game.i18n.localize('IRONSWORN.Foes'),
+			width: 400,
+			height: 600,
+			resizable: true,
+			rootComponent: FoeBrowserVue
+		}) as any
+	}
+}
---
+++ src/module/vue/foe-browser.vue
@@ -0,0 +1,96 @@
+<template>
+	<section
+		v-for="ruleset in rulesets"
+		:key="ruleset.title"
+		class="nogrow asset-ruleset"
+	>
+		<h1>{{ $t(`IRONSWORN.RULESETS.${ruleset}`) }}</h1>
+		<section
+			v-for="category in ruleset.categories"
+			:key="category.title"
+			class="nogrow foe-category"
+		>
+			<h2 class="flexrow">
+				<IronBtn
+					:aria-controls="category.title"
+					:text="category.title"
+					:icon="category.expanded ? 'fa:caret-down' : 'fa:caret-right'"
+					@click="category.expanded = !category.expanded"
+				/>
+			</h2>
+
+			<CollapseTransition>
+				<Suspense>
+					<div v-if="category.expanded">
+						<section
+							:id="category.title"
+							class="foe-category-contents"
+							:aria-expanded="category.expanded"
+						>
+							<RenderedText
+								v-if="category.description"
+								element="div"
+								class="category-description"
+								:content="category.description"
+								:markdown="true"
+							/>
+
+							<!-- TODO: FoeBrowserCard and FoeBrowserCardContents -->
+						</section>
+					</div>
+				</Suspense>
+			</CollapseTransition>
+		</section>
+	</section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import CollapseTransition from 'component:transition/collapse-transition.vue'
+import { IronswornSettings } from '../helpers/settings'
+import IronBtn from 'component:buttons/iron-btn.vue'
+import RenderedText from 'component:rendered-text.vue'
+
+// Not used, but this prevents a Vue warning
+defineProps<{ data: any }>()
+
+const rulesets = IronswornSettings.enabledRulesets
+</script>
+
+<style lang="scss" scoped>
+h1 {
+	margin: 0;
+	border: none;
+	height: min-content;
+	line-height: 1.5;
+	text-transform: uppercase;
+}
+
+h2 {
+	margin: 0;
+	border: none;
+	height: min-content;
+	line-height: 1.5;
+
+	button {
+		height: min-content;
+		text-transform: uppercase;
+		line-height: 1.5;
+	}
+}
+
+.foe-category {
+	margin-bottom: 1em;
+	border: var(--ironsworn-border-width-md) solid var(--ironsworn-color-border);
+	border-radius: var(--ironsworn-border-radius-lg);
+	padding: var(--ironsworn-spacer-md);
+}
+
+.foe-category-contents {
+	margin: var(--ironsworn-spacer-md) var(--ironsworn-spacer-xl);
+}
+
+.category-description {
+	padding-bottom: var(--ironsworn-spacer-xl);
+}
+</style>
