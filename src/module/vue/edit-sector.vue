ec095caafd43211aef7e5b9cb054fd3bb280a646
Ben Straub <ben@straub.cc>
2024-08-28 06:43:49 -0700

    Update edit-sector dialog

--- src/module/vue/edit-sector.vue
+++ src/module/vue/edit-sector.vue
@@ -4,10 +4,20 @@
 			{{ $t('IRONSWORN.Region') }}
 			<i class="fa fa-circle-question" data-tooltip="IRONSWORN.RegionTip"></i>
 		</h4>
-		<label class="nogrow" v-for="option in options">
-			<input v-model="region" type="radio" :value="option.toLowerCase()" />
-			{{ $t(`IRONSWORN.REGION.${option}`) }}
-		</label>
+
+		<div v-if="starforged" class="nogrow" :class="$style.grid">
+			<label class="nogrow" v-for="option in starforgedOptions">
+				<input v-model="region" type="radio" :value="option.toLowerCase()" />
+				{{ $t(`IRONSWORN.REGION.${option}`) }}
+			</label>
+		</div>
+
+		<div v-if="sunderedIsles" class="nogrow" :class="$style.grid">
+			<label class="nogrow" v-for="option in sunderedIslesOptions">
+				<input v-model="region" type="radio" :value="option.toLowerCase()" />
+				{{ $t(`IRONSWORN.REGION.${option}`) }}
+			</label>
+		</div>
 	</div>
 </template>
 
@@ -17,13 +27,12 @@ import { IronswornSettings } from '../helpers/settings'
 
 const props = defineProps<{ data: { sceneId: string } }>()
 
-const defaultToolbox = IronswornSettings.defaultToolbox
-const options: string[] =
-	defaultToolbox === 'starforged'
-		? ['Terminus', 'Outlands', 'Expanse']
-		: defaultToolbox === 'sunderedisles'
-		? ['Myriads', 'Margins', 'Reaches']
-		: []
+const starforged = IronswornSettings.enabledRulesets.includes('starforged')
+const starforgedOptions = ['Terminus', 'Outlands', 'Expanse']
+
+const sunderedIsles =
+	IronswornSettings.enabledRulesets.includes('sundered_isles')
+const sunderedIslesOptions = ['Myriads', 'Margins', 'Reaches']
 
 const scene = game.scenes?.get(props.data.sceneId)
 // @ts-expect-error scene.flags isn't in the types
@@ -34,3 +43,11 @@ watch(region, (newValue) => {
 	scene?.setFlag('foundry-ironsworn', 'region', newValue)
 })
 </script>
+
+<style lang="scss" module>
+.grid {
+	margin: 0.25rem 1rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
